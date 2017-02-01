602a4f7d63e2afd6a8a849834c8d2fda739030dc
kgtkr <kgtkr.jp+program@gmail.com>
2017-02-01 19:01:38 +0900

    configファイルをセキュティに関わる部分のみ外部ファイル化

--- config.ts
+++ config.ts
@@ -1,6 +1,6 @@
 import * as fs from 'fs';
 
-interface IConfig {
+interface IConfigFile {
     server: {
         port: number
     },
@@ -8,86 +8,6 @@ interface IConfig {
         url: string,
         user: string,
         pass: string
-    },
-    user: {
-        sn: {
-            regex: string,
-            msg: string
-        },
-        pass: {
-            regex: string,
-            msg: string
-        },
-        lvMax: number,
-        profile: {
-            name: {
-                regex: string,
-                msg: string
-            },
-            text: {
-                regex: string,
-                msg: string
-            },
-            sn: {
-                regex: string,
-                msg: string
-            }
-        },
-        token: {
-            name: {
-                regex: string,
-                msg: string
-            },
-            req: {
-                expireMinute: 5
-            }
-        },
-        client: {
-            name: {
-                regex: string,
-                msg: string
-            },
-            url: {
-                regex: string,
-                msg: string
-            }
-        }
-    },
-    topic: {
-        title: {
-            regex: string,
-            msg: string
-        },
-        tags: {
-            regex: string,
-            max: number,
-            msg: string
-        },
-        text: {
-            regex: string,
-            msg: string
-        }
-    },
-    res: {
-        defaultName: string,
-        name: {
-            regex: string,
-            msg: string
-        },
-        text: {
-            regex: string,
-            msg: string
-        },
-        wait: {
-            maxLv: number,
-            minSecond: number,
-            m10: number,
-            m30: number,
-            h1: number,
-            h6: number,
-            h12: number,
-            d1: number
-        }
     },
     salt: {
         pass: string,
@@ -97,4 +17,90 @@ interface IConfig {
     }
 }
 
-export const Config: IConfig = JSON.parse(fs.readFileSync("./config.json", "utf8"));
+
+let config:IConfigFile=JSON.parse(fs.readFileSync("./config.json", "utf8"));
+export const Config={
+    server: config.server,
+    db: config.db,
+    salt: config.salt,
+    user: {
+        sn: {
+            regex: "^[a-zA-Z0-9_]{3,20}$",
+            msg: "スクリーンネームは半角英数字、アンダーバー3～20文字にして下さい"
+        },
+        pass: {
+            regex: "^[a-zA-Z0-9_]{3,50}$",
+            msg: "パスワードは半角英数字、アンダーバー3～50文字にして下さい"
+        },
+        lvMax: 1000,
+        profile: {
+            name: {
+                regex: "^.{1,50}$",
+                msg: "名前は1～50文字にして下さい"
+            },
+            sn: {
+                regex: "^[a-zA-Z0-9_]{3,20}$",
+                msg: "スクリーンネームは半角英数字、アンダーバー3～20文字にして下さい"
+            },
+            text: {
+                regex: "^[\\s\\S]{1,3000}$",
+                msg: "自己紹介文は1～3000文字にして下さい"
+            }
+        },
+        token: {
+            name: {
+                regex: "^.{1,50}$",
+                msg: "名前は1～50文字にして下さい"
+            },
+            req: {
+                expireMinute: 5
+            }
+        },
+        client: {
+            name: {
+                regex: "^.{0,30}$",
+                msg: "名前は1～30文字にして下さい"
+            },
+            url: {
+                regex: "^https?:\\/\\/.{1,500}$",
+                msg: "URLが不正です"
+            }
+        }
+    },
+    topic: {
+        title: {
+            regex: "^.{1,100}$",
+            msg: "タイトルは1～100文字にして下さい"
+        },
+        tags: {
+            regex: "^[a-z0-9_]{1,15}$",
+            max: 10,
+            msg: "タグは半角小文字英数字、アンダーバー1～15文字10階層以内にして下さい"
+        },
+        text: {
+            regex: "^[\\s\\S]{1,10000}$",
+            msg: "本文は1～1万文字以内にして下さい"
+        }
+    },
+    res: {
+        defaultName: "anonymous",
+        name: {
+            regex: "^.{0,50}$",
+            msg: "名前は50文字以内にして下さい"
+        },
+        text: {
+            regex: "^[\\s\\S]{1,5000}$",
+            msg: "本文は1～5000文字にして下さい"
+        },
+        wait: {
+            maxLv: 3,
+            minSecond: 30,
+            m10: 5,
+            m30: 10,
+            h1: 15,
+            h6: 20,
+            h12: 35,
+            d1: 50
+        }
+    }
+};
