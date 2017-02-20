37b976455783449ce08700299d5aa222a18c1776
kgtkr <kgtkr.jp+program@gmail.com>
2017-02-20 23:39:31 +0900

    名前の記号エスケープバグ

--- util.ts
+++ util.ts
@@ -12,7 +12,7 @@ export class StringUtil {
             "●": "○",
             "■": "□",
             "▼": "▽",
-            "▲": "▲",
+            "▲": "△",
             "◆": "◇"
         };
         return str.replace(/[★●■▼▲◆]/g, function (match) {
