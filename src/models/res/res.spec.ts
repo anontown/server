88ba39fd944ca520be3305e1df3aa53ec8e2a1fe
kgtkr <kgtkr.jp@gmail.com>
2017-12-28 19:19:14 +0900

    テスト

--- src/models/res/res.spec.ts
+++ src/models/res/res.spec.ts
@@ -2,7 +2,9 @@ import {
   IAuthToken,
   IVote,
   ResBase,
+  User
 } from "../../";
+import { ObjectID } from "mongodb";
 
 describe("Res", () => {
   describe("ResBase", () => {
@@ -19,7 +21,11 @@ describe("Res", () => {
     describe("constructor", () => {
       it("正常に作れるか", () => {
         const date = new Date();
-        const vote: IVote[] = [];
+        const vote: IVote[] = [{
+          user: "user",
+          value: 5,
+          lv: 10,
+        }];
         const res = new ResBaseTest("id",
           "topic",
           date,
@@ -39,6 +45,71 @@ describe("Res", () => {
         expect(res.hash).toBe("hash");
         expect(res.type).toBe("normal");
         expect(res.replyCount).toBe(10);
+        expect(res.voteValue).toBe(5);
+      });
+    });
+    describe("#uv", () => {
+      it("正常に投票出来るか", () => {
+        const resUserID = new ObjectID();
+        const voteUserID = new ObjectID();
+        const date = new Date();
+        const vote: IVote[] = [];
+        const res = new ResBaseTest("id",
+          "topic",
+          date,
+          resUserID.toHexString(),
+          vote,
+          1,
+          "hash",
+          "normal",
+          10);
+        const resUser = User.fromDB({
+          _id: resUserID,
+          sn: "sn1",
+          pass: "pass",
+          lv: 2,
+          resWait: {
+            last: new Date(),
+            m10: 0,
+            m30: 0,
+            h1: 0,
+            h6: 0,
+            h12: 0,
+            d1: 0,
+          },
+          lastTopic: new Date(),
+          date: new Date(),
+          point: 1,
+          lastOneTopic: new Date()
+        });
+        res.uv(resUser,
+          User.fromDB({
+            _id: voteUserID,
+            sn: "sn1",
+            pass: "pass",
+            lv: 2,
+            resWait: {
+              last: new Date(),
+              m10: 0,
+              m30: 0,
+              h1: 0,
+              h6: 0,
+              h12: 0,
+              d1: 0,
+            },
+            lastTopic: new Date(),
+            date: new Date(),
+            point: 1,
+            lastOneTopic: new Date()
+          }),
+          {
+            id: voteUserID.toHexString(),
+            key: "aaaaa",
+            user: "user2",
+            type: "master"
+          });
+        expect(res.vote).toEqual([{ user: voteUserID.toHexString(), value: 2, lv: 2 }]);
+        expect(resUser.lv)
       });
     });
   });
