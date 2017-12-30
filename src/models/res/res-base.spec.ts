a4f8a0944c9e3911fd5fb94f0321d01af853f230
kgtkr <kgtkr.jp@gmail.com>
2017-12-30 10:35:14 +0900

    ちょっと修正

--- src/models/res/res-base.spec.ts
+++ src/models/res/res-base.spec.ts
@@ -9,211 +9,209 @@ import {
 } from "../../";
 import { ObjectID } from "mongodb";
 
-describe("Res", () => {
-  describe("ResBase", () => {
-    class ResBaseTest extends ResBase<"normal"> {
-      toDB() {
-        return super.toBaseDB({});
-      }
-
-      toAPI(authToken: IAuthToken | null) {
-        return super.toBaseAPI(authToken);
-      }
+describe("ResBase", () => {
+  class ResBaseTest extends ResBase<"normal"> {
+    toDB() {
+      return super.toBaseDB({});
     }
 
-    describe("constructor", () => {
-      it("正常に作れるか", () => {
-        const date = new Date();
-        const vote: IVote[] = [{
-          user: "user",
-          value: 5,
-        }];
-        const res = new ResBaseTest("id",
-          "topic",
-          date,
-          "user",
-          vote,
-          1,
-          "hash",
-          "normal",
-          10);
+    toAPI(authToken: IAuthToken | null) {
+      return super.toBaseAPI(authToken);
+    }
+  }
 
-        expect(res.id).toBe("id");
-        expect(res.topic).toBe("topic");
-        expect(res.date).toBe(date);
-        expect(res.user).toBe("user");
-        expect(res.vote).toBe(vote);
-        expect(res.lv).toBe(1);
-        expect(res.hash).toBe("hash");
-        expect(res.type).toBe("normal");
-        expect(res.replyCount).toBe(10);
-      });
+  describe("constructor", () => {
+    it("正常に作れるか", () => {
+      const date = new Date();
+      const vote: IVote[] = [{
+        user: "user",
+        value: 5,
+      }];
+      const res = new ResBaseTest("id",
+        "topic",
+        date,
+        "user",
+        vote,
+        1,
+        "hash",
+        "normal",
+        10);
+
+      expect(res.id).toBe("id");
+      expect(res.topic).toBe("topic");
+      expect(res.date).toBe(date);
+      expect(res.user).toBe("user");
+      expect(res.vote).toBe(vote);
+      expect(res.lv).toBe(1);
+      expect(res.hash).toBe("hash");
+      expect(res.type).toBe("normal");
+      expect(res.replyCount).toBe(10);
     });
+  });
 
-    describe("#v", () => {
-      it("正常に投票出来るか", () => {
-        const resWait: IResWait = {
-          last: new Date(),
-          m10: 0,
-          m30: 0,
-          h1: 0,
-          h6: 0,
-          h12: 0,
-          d1: 0,
-        };
-        const resUserID = ObjectIDGenerator.get();
-        const voteUserID = ObjectIDGenerator.get();
-        const date = new Date();
-        const vote: IVote[] = [];
-        const res = new ResBaseTest("id",
-          "topic",
-          date,
-          resUserID,
-          vote,
-          1,
-          "hash",
-          "normal",
-          10);
-        const resUser = User.fromDB({
-          _id: new ObjectID(resUserID),
+  describe("#v", () => {
+    it("正常に投票出来るか", () => {
+      const resWait: IResWait = {
+        last: new Date(),
+        m10: 0,
+        m30: 0,
+        h1: 0,
+        h6: 0,
+        h12: 0,
+        d1: 0,
+      };
+      const resUserID = ObjectIDGenerator.get();
+      const voteUserID = ObjectIDGenerator.get();
+      const date = new Date();
+      const vote: IVote[] = [];
+      const res = new ResBaseTest("id",
+        "topic",
+        date,
+        resUserID,
+        vote,
+        1,
+        "hash",
+        "normal",
+        10);
+      const resUser = User.fromDB({
+        _id: new ObjectID(resUserID),
+        sn: "sn1",
+        pass: "pass",
+        lv: 2,
+        resWait,
+        lastTopic: new Date(),
+        date: new Date(),
+        point: 1,
+        lastOneTopic: new Date()
+      });
+      res.v(resUser,
+        User.fromDB({
+          _id: new ObjectID(voteUserID),
           sn: "sn1",
           pass: "pass",
-          lv: 2,
+          lv: 101,
           resWait,
           lastTopic: new Date(),
           date: new Date(),
           point: 1,
           lastOneTopic: new Date()
+        }),
+        "uv",
+        {
+          id: voteUserID,
+          key: "aaaaa",
+          user: "user2",
+          type: "master"
         });
+
+      expect(res.vote).toEqual([{ user: voteUserID, value: 2 }]);
+      expect(resUser.lv).toBe(4);
+    });
+
+    it("自分に投票するとエラーになるか", () => {
+      const resWait: IResWait = {
+        last: new Date(),
+        m10: 0,
+        m30: 0,
+        h1: 0,
+        h6: 0,
+        h12: 0,
+        d1: 0,
+      };
+      const resUserID = ObjectIDGenerator.get();
+      const date = new Date();
+      const vote: IVote[] = [];
+      const res = new ResBaseTest("id",
+        "topic",
+        date,
+        resUserID,
+        vote,
+        1,
+        "hash",
+        "normal",
+        10);
+      const resUser = User.fromDB({
+        _id: new ObjectID(resUserID),
+        sn: "sn1",
+        pass: "pass",
+        lv: 2,
+        resWait,
+        lastTopic: new Date(),
+        date: new Date(),
+        point: 1,
+        lastOneTopic: new Date()
+      });
+      expect(() => {
         res.v(resUser,
-          User.fromDB({
-            _id: new ObjectID(voteUserID),
-            sn: "sn1",
-            pass: "pass",
-            lv: 101,
-            resWait,
-            lastTopic: new Date(),
-            date: new Date(),
-            point: 1,
-            lastOneTopic: new Date()
-          }),
+          resUser,
           "uv",
           {
-            id: voteUserID,
+            id: resUserID,
             key: "aaaaa",
             user: "user2",
             type: "master"
           });
+      }).toThrow(AtError);
+    });
 
-        expect(res.vote).toEqual([{ user: voteUserID, value: 2 }]);
-        expect(resUser.lv).toBe(4);
+    it("重複投票でエラーになるか", () => {
+      const resWait: IResWait = {
+        last: new Date(),
+        m10: 0,
+        m30: 0,
+        h1: 0,
+        h6: 0,
+        h12: 0,
+        d1: 0,
+      };
+      const resUserID = ObjectIDGenerator.get();
+      const voteUserID = ObjectIDGenerator.get();
+      const date = new Date();
+      const vote: IVote[] = [];
+      const res = new ResBaseTest("id",
+        "topic",
+        date,
+        resUserID,
+        vote,
+        1,
+        "hash",
+        "normal",
+        10);
+      const resUser = User.fromDB({
+        _id: new ObjectID(resUserID),
+        sn: "sn1",
+        pass: "pass",
+        lv: 2,
+        resWait,
+        lastTopic: new Date(),
+        date: new Date(),
+        point: 1,
+        lastOneTopic: new Date()
       });
-
-      it("自分に投票するとエラーになるか", () => {
-        const resWait: IResWait = {
-          last: new Date(),
-          m10: 0,
-          m30: 0,
-          h1: 0,
-          h6: 0,
-          h12: 0,
-          d1: 0,
-        };
-        const resUserID = ObjectIDGenerator.get();
-        const date = new Date();
-        const vote: IVote[] = [];
-        const res = new ResBaseTest("id",
-          "topic",
-          date,
-          resUserID,
-          vote,
-          1,
-          "hash",
-          "normal",
-          10);
-        const resUser = User.fromDB({
-          _id: new ObjectID(resUserID),
-          sn: "sn1",
-          pass: "pass",
-          lv: 2,
-          resWait,
-          lastTopic: new Date(),
-          date: new Date(),
-          point: 1,
-          lastOneTopic: new Date()
-        });
-        expect(() => {
+      expect(() => {
+        for (let i = 0; i < 2; i++) {
           res.v(resUser,
-            resUser,
+            User.fromDB({
+              _id: new ObjectID(voteUserID),
+              sn: "sn1",
+              pass: "pass",
+              lv: 101,
+              resWait,
+              lastTopic: new Date(),
+              date: new Date(),
+              point: 1,
+              lastOneTopic: new Date()
+            }),
             "uv",
             {
-              id: resUserID,
+              id: voteUserID,
               key: "aaaaa",
               user: "user2",
               type: "master"
             });
-        }).toThrow(AtError);
-      });
-
-      it("重複投票でエラーになるか", () => {
-        const resWait: IResWait = {
-          last: new Date(),
-          m10: 0,
-          m30: 0,
-          h1: 0,
-          h6: 0,
-          h12: 0,
-          d1: 0,
-        };
-        const resUserID = ObjectIDGenerator.get();
-        const voteUserID = ObjectIDGenerator.get();
-        const date = new Date();
-        const vote: IVote[] = [];
-        const res = new ResBaseTest("id",
-          "topic",
-          date,
-          resUserID,
-          vote,
-          1,
-          "hash",
-          "normal",
-          10);
-        const resUser = User.fromDB({
-          _id: new ObjectID(resUserID),
-          sn: "sn1",
-          pass: "pass",
-          lv: 2,
-          resWait,
-          lastTopic: new Date(),
-          date: new Date(),
-          point: 1,
-          lastOneTopic: new Date()
-        });
-        expect(() => {
-          for (let i = 0; i < 2; i++) {
-            res.v(resUser,
-              User.fromDB({
-                _id: new ObjectID(voteUserID),
-                sn: "sn1",
-                pass: "pass",
-                lv: 101,
-                resWait,
-                lastTopic: new Date(),
-                date: new Date(),
-                point: 1,
-                lastOneTopic: new Date()
-              }),
-              "uv",
-              {
-                id: voteUserID,
-                key: "aaaaa",
-                user: "user2",
-                type: "master"
-              });
-          }
-        }).toThrow(AtError);
-      });
+        }
+      }).toThrow(AtError);
     });
-
   });
+
 });
