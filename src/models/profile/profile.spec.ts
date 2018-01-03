c7903a09c65127b767297b755d9f77453233b0a2
kgtkr <kgtkr.jp@gmail.com>
2018-01-03 14:23:31 +0900

    profileのテスト修正

--- src/models/profile/profile.spec.ts
+++ src/models/profile/profile.spec.ts
@@ -1,4 +1,5 @@
 import { ObjectID } from "mongodb";
+
 import {
   ObjectIDGenerator,
   Profile,
@@ -19,13 +20,13 @@ describe("Profile", () => {
       it("正常に変換できるか", () => {
         const db = profile.toDB();
 
-        expect(db._id.toHexString()).toBe(profile.id);
-        expect(db.user.toHexString()).toBe(profile.user);
+        expect(db._id).toEqual(new ObjectID(profile.id));
+        expect(db.user).toEqual(new ObjectID(profile.user));
         expect(db.name).toBe(profile.name);
         expect(db.body).toBe(profile.body);
-        expect(db.date.valueOf()).toBe(profile.date.valueOf());
-        expect(db.update.valueOf()).toBe(profile.update.valueOf());
-        expect(db.sn === profile.sn);
+        expect(db.date).toEqual(profile.date);
+        expect(db.update).toEqual(profile.update);
+        expect(db.sn).toEqual(profile.sn);
       });
     });
 
@@ -71,19 +72,20 @@ describe("Profile", () => {
     it("正常に作れるか", () => {
       const user = new ObjectID().toString();
       const id = new ObjectID().toString();
-      const date = Date.now();
+      const date = new Date();
       const profile = Profile.create(() => id, {
         type: "master",
         user,
         key: "",
         id: ObjectIDGenerator(),
-      }, "名前", "本文", "test", new Date(date));
-      expect(profile.id.toString()).toBe(id);
-      expect(profile.user.toString()).toBe(user);
+      }, "名前", "本文", "test", date);
+
+      expect(profile.id).toBe(id);
+      expect(profile.user).toBe(user);
       expect(profile.name).toBe("名前");
       expect(profile.body).toBe("本文");
-      expect(profile.date.valueOf()).toBe(date);
-      expect(profile.update.valueOf()).toBe(date);
+      expect(profile.date).toEqual(date);
+      expect(profile.update).toEqual(date);
       expect(profile.sn).toBe("test");
     });
   });
