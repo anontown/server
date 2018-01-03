5a05e3d6d730ee95863c91b9193a3fccbaf4d331
kgtkr <kgtkr.jp@gmail.com>
2018-01-03 17:36:47 +0900

    不変に

--- src/auth.ts
+++ src/auth.ts
@@ -1,17 +1,17 @@
 export interface IAuthTokenBase<T extends "master" | "general"> {
-    id: string;
-    key: string;
-    user: string;
-    type: T;
+    readonly id: string;
+    readonly key: string;
+    readonly user: string;
+    readonly type: T;
 }
 
 export type IAuthTokenMaster = IAuthTokenBase<"master">;
 export interface IAuthTokenGeneral extends IAuthTokenBase<"general"> {
-    client: string;
+    readonly client: string;
 }
 export type IAuthToken = IAuthTokenMaster | IAuthTokenGeneral;
 
 export interface IAuthUser {
-    id: string;
-    pass: string;
+    readonly id: string;
+    readonly pass: string;
 }
