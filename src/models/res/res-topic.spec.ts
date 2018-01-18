f073dcea4d4f567ffaf947134d7f48489922f995
kgtkr <kgtkr.jp@gmail.com>
2018-01-18 23:39:48 +0900

    テスト追加

--- src/models/res/res-topic.spec.ts
+++ src/models/res/res-topic.spec.ts
@@ -1,10 +1,51 @@
 import * as Im from "immutable";
 import {
   ResTopic,
-  IResTopicDB
+  IResTopicDB,
+  TopicOne,
+  User,
+  IAuthToken
 } from "../../";
 
 describe("ResTopic", () => {
+  const topicOne = new TopicOne(
+    "topic",
+    "title",
+    Im.List(),
+    "body",
+    new Date(),
+    new Date(),
+    10,
+    new Date(),
+    true,
+  );
+
+  const user = new User(
+    "user",
+    "sn",
+    "pass",
+    1,
+    {
+      last: new Date(0),
+      m10: 0,
+      m30: 0,
+      h1: 0,
+      h6: 0,
+      h12: 0,
+      d1: 0,
+    },
+    new Date(),
+    new Date(),
+    0,
+    new Date());
+
+  const token: IAuthToken = {
+    id: "token",
+    key: "key",
+    user: "user",
+    type: "master",
+  };
+
   describe("fromDB", () => {
     it("正常に作れるか", () => {
       const db: IResTopicDB = {
@@ -33,5 +74,22 @@ describe("ResTopic", () => {
       expect(res.hash).toBe(db.body.hash);
       expect(res.replyCount).toBe(replyCount);
     });
+  });
+
+  describe("create", () => {
+    const date = new Date();
+    const { res, topic } = ResTopic.create(() => "res", topicOne, user, token, date);
+
+    expect(res.type).toBe("topic");
+    expect(res.id).toBe("res");
+    expect(res.topic).toBe("topic");
+    expect(res.date).toEqual(date);
+    expect(res.user).toBe("user");
+    expect(res.vote).toEqual(Im.List());
+    expect(res.lv).toBe(5);
+    expect(res.hash).toBe(topicOne.hash(date, user));
+    expect(res.replyCount).toBe(0);
+
+    expect(topic.update).toEqual(date);
   });
 });
