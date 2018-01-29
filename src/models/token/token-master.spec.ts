e70d67ff473f0798adedd400ed71156d4606b452
kgtkr <kgtkr.jp@gmail.com>
2018-01-29 21:24:31 +0900

    テスト追加

---
+++ src/models/token/token-master.spec.ts
@@ -0,0 +1,28 @@
+import {
+  TokenMaster,
+  ITokenMasterDB,
+  ObjectIDGenerator
+} from "../../";
+import { ObjectID } from "mongodb";
+import { take } from "rxjs/operator/take";
+
+describe("TokenMaster", () => {
+  describe("fromDB", () => {
+    it("正常に変換出来るか", () => {
+      const db: ITokenMasterDB = {
+        _id: new ObjectID(ObjectIDGenerator()),
+        key: "key",
+        type: "master",
+        user: new ObjectID(ObjectIDGenerator()),
+        date: new Date(),
+      };
+      const token = TokenMaster.fromDB(db);
+
+      expect(token.id).toBe(db._id.toHexString());
+      expect(token.key).toBe(db.key);
+      expect(token.type).toBe(db.type);
+      expect(token.user).toBe(db.user.toHexString());
+      expect(token.date).toEqual(token.date);
+    });
+  });
+});
