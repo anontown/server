e92a5f3b2189c93950aeb37a86267860b0ffea36
kgtkr <kgtkr.jp@gmail.com>
2018-02-10 18:46:14 +0900

    テスト追加

--- src/models/topic/topic-base.spec.ts
+++ src/models/topic/topic-base.spec.ts
@@ -1,6 +1,6 @@
 import Copyable from "ts-copyable";
 import {
-  ObjectIDGenerator,
+  AtError,
   TopicBase,
   applyMixins,
   ITopicBaseAPI,
@@ -30,4 +30,67 @@ describe("TopicBase", () => {
   }
   applyMixins(TopicBaseTest, [TopicBase]);
 
+  describe("checkData", () => {
+    it("正常に動くか", () => {
+      TopicBase.checkData({ title: "title", tags: ["a", "b"], body: "body" });
+      TopicBase.checkData({ title: "title", tags: [], body: "body" });
+    });
+
+    it("タイトルが長すぎるとエラーになるか", () => {
+      expect(() => {
+        TopicBase.checkData({ title: "x".repeat(101) });
+      }).toThrow(AtError);
+    });
+
+    it("タグの文字が不正だとエラーになるか", () => {
+      expect(() => {
+        TopicBase.checkData({ tags: ["a", "a b"] });
+      }).toThrow(AtError);
+
+      expect(() => {
+        TopicBase.checkData({ tags: ["x", ""] });
+      }).toThrow(AtError);
+
+      expect(() => {
+        TopicBase.checkData({ tags: ["a".repeat(21), "a"] });
+      }).toThrow(AtError);
+    });
+
+    it("タグに重複があるとエラーになるか", () => {
+      expect(() => {
+        TopicBase.checkData({ tags: ["a", "b", "a"] });
+      });
+    });
+
+    it("タグの数が多すぎるとエラーになるか", () => {
+      expect(() => {
+        TopicBase.checkData({
+          tags: [
+            "a",
+            "b",
+            "c",
+            "d",
+            "e",
+            "f",
+            "g",
+            "h",
+            "i",
+            "j",
+            "k",
+            "l",
+            "m",
+            "n",
+            "o",
+            "p"
+          ]
+        });
+      });
+    });
+
+    it("本文が長すぎるとエラーになるか", () => {
+      expect(() => {
+        TopicBase.checkData({ body: "x".repeat(10001) });
+      }).toThrow(AtError);
+    });
+  });
 });
