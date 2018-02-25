35146aaf7dca7a86ca041020353e21e5b4801885
kgtkr <kgtkr.jp@gmail.com>
2018-02-25 20:12:47 +0900

    テスト改善

--- src/models/res/res-history.spec.ts
+++ src/models/res/res-history.spec.ts
@@ -61,7 +61,7 @@ describe("ResHistory", () => {
 
   describe("fromDB", () => {
     it("正常に作れるか", () => {
-      const db: IResHistoryDB = {
+      expect(ResHistory.fromDB({
         id: "id",
         type: "history",
         body: {
@@ -73,48 +73,39 @@ describe("ResHistory", () => {
           hash: "hash",
           history: "history",
         },
-      };
-      const replyCount = 3;
-
-      const res = ResHistory.fromDB(db, replyCount);
-
-      expect(res.type).toBe(db.type);
-      expect(res.history).toBe(db.body.history);
-      expect(res.id).toBe(db.id);
-      expect(res.topic).toBe(db.body.topic);
-      expect(res.date).toEqual(new Date(db.body.date));
-      expect(res.user).toBe(db.body.user);
-      expect(res.vote).toEqual(Im.List(db.body.vote));
-      expect(res.lv).toBe(db.body.lv);
-      expect(res.hash).toBe(db.body.hash);
-      expect(res.replyCount).toBe(replyCount);
+      }, 3)).toEqual(new ResHistory("history",
+        "id",
+        "topic",
+        new Date(1000),
+        "user",
+        Im.List(),
+        5,
+        "hash",
+        3));
     });
   });
 
   describe("create", () => {
     it("正常に作れるか", () => {
-      const date = new Date(1000);
       const { res, topic: newTopic } = ResHistory.create(
         () => "res",
         topicNormal,
         user,
         token,
         history,
-        date);
+        new Date(1000));
 
-      expect(res.type).toBe("history");
-      expect(res.history).toBe("history");
-      expect(res.id).toBe("res");
-      expect(res.topic).toBe("topic");
-      expect(res.date).toEqual(date);
-      expect(res.user).toBe("user");
-      expect(res.vote).toEqual(Im.List());
-      expect(res.lv).toBe(user.lv * 5);
-      expect(res.hash).toBe(topicNormal.hash(date, user));
-      expect(res.replyCount).toBe(0);
+      expect(res).toEqual(new ResHistory("history",
+        "res",
+        "topic",
+        new Date(1000),
+        "user",
+        Im.List(),
+        1,
+        topicNormal.hash(new Date(1000), user),
+        0));
 
-      expect(newTopic.update).toEqual(date);
-      expect(newTopic.ageUpdate).toEqual(topicNormal.ageUpdate);
+      expect(newTopic).toEqual(topicNormal.copy({ update: new Date(1000) }));
     });
   });
 
