059f419c62f7fffcb2d512a361d75e6081d53a5c
kgtkr <kgtkr.jp@gmail.com>
2018-02-25 17:48:54 +0900

    テスト改善

--- src/models/msg/msg.spec.ts
+++ src/models/msg/msg.spec.ts
@@ -4,116 +4,83 @@ import {
   Msg,
   ObjectIDGenerator,
   User,
+  IAuthTokenMaster
 } from "../../";
 
 describe("Msg", () => {
-  {
-    const msg = Msg.fromDB({
-      id: ObjectIDGenerator(),
-      body: {
+  const msg = new Msg("msg", "user", "body", new Date(0));
+  const receiverNullMsg = new Msg("msg", null, "body", new Date(0));
+  const auth: IAuthTokenMaster = {
+    id: "token",
+    key: "key",
+    user: "user",
+    type: "master",
+  };
+
+  describe("#toDB", () => {
+    it("正常に変換出来るか", () => {
+      expect(msg.toDB()).toEqual({
+        id: "msg",
+        body: {
+          receiver: "user",
+          body: "body",
+          date: new Date(0).toISOString()
+        }
+      });
+    });
+  });
+
+  describe("#toAPI", () => {
+    it("正常に変換出来るか", () => {
+      expect(msg.toAPI(auth)).toEqual({
+        id: "msg",
         receiver: "user",
-        body: "あいうえお",
-        date: new Date(0).toISOString(),
-      },
+        body: "body",
+        date: new Date(0).toISOString()
+      });
     });
 
-    const receiverNullMsg = Msg.fromDB({
-      id: ObjectIDGenerator(),
-      body: {
+    it("receiverがnull", () => {
+      expect(receiverNullMsg.toAPI(auth)).toEqual({
+        id: "msg",
         receiver: null,
-        body: "あいうえお",
-        date: new Date(0).toISOString(),
-      },
-    });
-
-    describe("#toDB", () => {
-      it("正常に変換出来るか", () => {
-        const db = msg.toDB();
-
-        expect(db.id).toBe(msg.id);
-        expect(db.body.receiver).toBe(msg.receiver);
-        expect(db.body.body).toBe(msg.body);
-        expect(db.body.date).toBe(msg.date.toISOString());
+        body: "body",
+        date: new Date(0).toISOString()
       });
     });
 
-    describe("#toAPI", () => {
-      it("正常に変換出来るか", () => {
-        const api = msg.toAPI({
-          id: "usid",
-          key: "aaa",
-          user: "user",
-          type: "master",
-        });
-
-        expect(api.id).toBe(msg.id);
-        expect(api.receiver).toBe(msg.receiver);
-        expect(api.body).toBe(msg.body);
-        expect(api.date).toBe(msg.date.toISOString());
-      });
-
-      it("receiverがnull", () => {
-        const api = receiverNullMsg.toAPI({
-          id: "usid",
-          key: "aaa",
-          user: "user",
-          type: "master",
-        });
-
-        expect(api.receiver).toBeNull();
-      });
-
-      it("receiverがnot nullかつユーザーが違う時エラーになるか", () => {
-        expect(() => {
-          msg.toAPI({
-            id: "usid",
-            key: "aaa",
-            user: "other",
-            type: "master",
-          });
-        }).toThrow(AtError);
-      });
+    it("receiverがnot nullかつユーザーが違う時エラーになるか", () => {
+      expect(() => {
+        msg.toAPI({ ...auth, user: "user2" });
+      }).toThrow(AtError);
     });
-  }
+  });
 
   describe("fromDB", () => {
     it("正常に作成できるか", () => {
-      const db: IMsgDB = {
-        id: ObjectIDGenerator(),
+      expect(Msg.fromDB({
+        id: "msg",
         body: {
-          receiver: ObjectIDGenerator(),
-          body: "a",
-          date: new Date(0).toISOString(),
-        },
-      };
-
-      const msg = Msg.fromDB(db);
-
-      expect(msg.id).toBe(db.id);
-      expect(msg.receiver).toBe(db.body.receiver);
-      expect(msg.body).toBe(db.body.body);
-      expect(msg.date).toEqual(new Date(db.body.date));
+          receiver: "user",
+          body: "body",
+          date: new Date(0).toISOString()
+        }
+      })).toBe(msg);
     });
   });
 
   describe("create", () => {
     it("receiverがnullの時正常に生成できるか", () => {
-      const date = new Date(0);
-      const msg = Msg.create(() => "msg", null, "hoge", date);
-
-      expect(msg.id).toBe("msg");
-      expect(msg.receiver).toBeNull();
-      expect(msg.body).toBe("hoge");
-      expect(msg.date).toEqual(date);
+      expect(Msg.create(() => "msg", null, "body", new Date(0))).toBe(receiverNullMsg);
     });
 
     it("receiverがnullでない時正常に生成出来るか", () => {
-      const date = new Date(0);
       const user = new User(
         "user",
         "sn",
         "pass",
-        1, {
+        1,
+        {
           last: new Date(100),
           m10: 0,
           m30: 0,
@@ -126,8 +93,7 @@ describe("Msg", () => {
         new Date(20),
         0,
         new Date(250));
-      const msg = Msg.create(() => "msg", user, "hoge", date);
-      expect(msg.receiver).toBe("user");
+      expect(Msg.create(() => "msg", user, "body", new Date(0))).toBe(msg);
     });
   });
 });
