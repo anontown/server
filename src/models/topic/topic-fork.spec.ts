fc73005177b6d3b265730a95a93e606fac0f7f27
kgtkr <kgtkr.jp@gmail.com>
2018-02-27 20:58:10 +0900

    テスト追加

--- src/models/topic/topic-fork.spec.ts
+++ src/models/topic/topic-fork.spec.ts
@@ -4,6 +4,8 @@ import {
   IAuthTokenMaster,
   TopicNormal,
   User,
+  ResTopic,
+  ResFork
 } from "../../";
 
 describe("TopicFork", () => {
@@ -15,6 +17,41 @@ describe("TopicFork", () => {
     new Date(50),
     true,
     "parent");
+
+  const user = new User("user",
+    "sn",
+    "pass",
+    10,
+    {
+      last: new Date(0),
+      m10: 0,
+      m30: 0,
+      h1: 0,
+      h6: 0,
+      h12: 0,
+      d1: 0,
+    },
+    new Date(0),
+    new Date(0),
+    0,
+    new Date(0));
+
+  const auth: IAuthTokenMaster = {
+    id: "token",
+    key: "key",
+    user: "user",
+    type: "master",
+  };
+
+  const parent = new TopicNormal("parent",
+    "title",
+    Im.List(),
+    "body",
+    new Date(100),
+    new Date(0),
+    5,
+    new Date(50),
+    true);
 
   describe("fromDB", () => {
     it("正常に生成できるか", () => {
@@ -30,6 +67,47 @@ describe("TopicFork", () => {
           parent: "parent"
         }
       }, 5)).toEqual(topicFork);
+    });
+  });
+
+  describe("create", () => {
+    it("正常に生成できるか", () => {
+      expect(TopicFork.create(() => "id",
+        "title",
+        parent,
+        user,
+        auth,
+        new Date(24 * 60 * 60 * 1000))).toEqual({
+          topic: topicFork.copy({
+            id: "id",
+            date: new Date(24 * 60 * 60 * 1000),
+            update: new Date(24 * 60 * 60 * 1000),
+            ageUpdate: new Date(24 * 60 * 60 * 1000)
+          }),
+          res: new ResTopic("id",
+            "id",
+            new Date(24 * 60 * 60 * 1000),
+            "user",
+            Im.List(),
+            50,
+            topicFork.copy({
+              id: "id"
+            }).hash(new Date(24 * 60 * 60 * 1000), user),
+            0),
+          resParent: new ResFork("id",
+            "id",
+            "topic",
+            new Date(24 * 60 * 60 * 1000),
+            "user",
+            Im.List(),
+            50,
+            parent.hash(new Date(24 * 60 * 60 * 1000), user),
+            0),
+          user: user.copy({
+            lastOneTopic: new Date(24 * 60 * 60 * 1000)
+          }),
+          parent: parent
+        });
     });
   });
 });
