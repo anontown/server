41238675aeb3cf8c16cde95ae71813bf84255ef4
kgtkr <kgtkr.jp@gmail.com>
2018-02-27 19:05:32 +0900

    テスト追加

--- src/models/topic/topic-one.spec.ts
+++ src/models/topic/topic-one.spec.ts
@@ -3,6 +3,7 @@ import {
   IAuthTokenMaster,
   TopicOne,
   User,
+  ResTopic
 } from "../../";
 
 describe("TopicOne", () => {
@@ -56,6 +57,36 @@ describe("TopicOne", () => {
           body: "body",
         }
       }, 5)).toEqual(topic);
+    });
+  });
+
+  describe("create", () => {
+    it("正常に生成できるか", () => {
+      expect(TopicOne.create(() => "id",
+        "title",
+        [],
+        "body",
+        user,
+        auth,
+        new Date(24 * 60 * 60 * 1000))).toEqual({
+          topic: topic.copy({
+            resCount: 1,
+            id: "id",
+            date: new Date(24 * 60 * 60 * 1000),
+            update: new Date(24 * 60 * 60 * 1000),
+            ageUpdate: new Date(24 * 60 * 60 * 1000)
+          }),
+          res: new ResTopic("id",
+            "id",
+            new Date(24 * 60 * 60 * 1000),
+            "user",
+            Im.List(),
+            50,
+            topic.hash(new Date(24 * 60 * 60 * 1000), user),
+            0
+          ),
+          user: user.copy({ lastOneTopic: new Date(24 * 60 * 60 * 1000) }),
+        });
     });
   });
 });
