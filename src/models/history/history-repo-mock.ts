5dffa72a1ba06eaf923ac2f53010d6344a13f39b
kgtkr <kgtkr.jp@gmail.com>
2018-03-01 19:23:39 +0900

    HistoryRepoMock

---
+++ src/models/history/history-repo-mock.ts
@@ -0,0 +1,49 @@
+import { AtNotFoundError, AtNotFoundPartError } from "../../at-error";
+import { Topic } from "../topic";
+import { History, IHistoryDB } from "./history";
+import { IHistoryRepo } from "./ihistory-repo";
+
+export class HistoryRepoMock implements IHistoryRepo {
+  private histories: IHistoryDB[] = [];
+
+  async insert(history: History): Promise<null> {
+    this.histories.push(history.toDB());
+    return null;
+  }
+
+  async update(history: History): Promise<null> {
+    this.histories[this.histories.findIndex(h => h.id === history.id)] = history.toDB();
+    return null;
+  }
+
+  async findOne(id: string): Promise<History> {
+    const history = this.histories.find(h => h.id === id);
+
+    if (history === undefined) {
+      throw new AtNotFoundError("編集履歴が存在しません");
+    }
+
+    return History.fromDB(history);
+  }
+
+  async findIn(ids: string[]): Promise<History[]> {
+    const histories = this.histories
+      .filter(h => ids.findIndex(id => h.id === id))
+      .sort((a, b) => new Date(a.body.date).valueOf() - new Date(b.body.date).valueOf());
+
+    if (histories.length !== ids.length) {
+      throw new AtNotFoundPartError("編集履歴が存在しません",
+        histories.map(x => x.id));
+    }
+
+    return histories.map(h => History.fromDB(h));
+  }
+
+  async findAll(topic: Topic): Promise<History[]> {
+    const histories = this.histories
+      .filter(x => x.body.topic === topic.id)
+      .sort((a, b) => new Date(a.body.date).valueOf() - new Date(b.body.date).valueOf());;
+
+    return histories.map(h => History.fromDB(h));
+  }
+}
--- src/models/history/index.ts
+++ src/models/history/index.ts
@@ -1,3 +1,4 @@
 export * from "./history";
 export * from "./history-repo";
 export * from "./ihistory-repo";
+export * from "./history-repo-mock";
