7257c3c9a7877ea4d3dbc7e751ff7d791fc1b2ad
kgtkr <kgtkr.jp@gmail.com>
2018-03-01 17:42:25 +0900

    index.ts

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,21 +1,8 @@
-import { AppServer } from "../server";
-
-import { addClientAPI } from "./client";
-import { addHistoryAPI } from "./history";
-import { addMsgAPI } from "./msg";
-import { addProfileAPI } from "./profile";
-import { addResAPI } from "./res";
-import { addTokenAPI } from "./token";
-import { addTopicAPI } from "./topic";
-import { addUserAPI } from "./user";
-
-export function addAPI(api: AppServer) {
-  addClientAPI(api);
-  addHistoryAPI(api);
-  addMsgAPI(api);
-  addProfileAPI(api);
-  addResAPI(api);
-  addTokenAPI(api);
-  addTopicAPI(api);
-  addUserAPI(api);
-}
+export * from "./client";
+export * from "./history";
+export * from "./msg";
+export * from "./profile";
+export * from "./res";
+export * from "./token";
+export * from "./topic";
+export * from "./user";
