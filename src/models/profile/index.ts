216b5b7bddaf4c704bcf7434a5ad91a7e4753ec2
kgtkr <kgtkr.jp@gmail.com>
2018-03-02 23:11:07 +0900

    ProfileRepoMock

--- src/models/profile/index.ts
+++ src/models/profile/index.ts
@@ -1,3 +1,4 @@
 export * from "./profile";
 export * from "./profile-repo";
 export * from "./iprofile-repo";
+export * from "./profile-repo-mock";
---
+++ src/models/profile/profile-repo-mock.ts
@@ -0,0 +1,49 @@
+import { AtNotFoundError, AtNotFoundPartError } from "../../at-error";
+import { IAuthToken } from "../../auth";
+import { IProfileRepo } from "./iprofile-repo";
+import { IProfileDB, Profile } from "./profile";
+
+export class ProfileRepoMock implements IProfileRepo {
+  private profiles: IProfileDB[] = [];
+
+  async findOne(id: string): Promise<Profile> {
+    const profile = this.profiles.find(x => x._id.toHexString() === id);
+
+    if (profile === undefined) {
+      throw new AtNotFoundError("プロフィールが存在しません");
+    }
+
+    return Profile.fromDB(profile);
+  }
+
+  async findIn(ids: string[]): Promise<Profile[]> {
+    const profiles = this.profiles
+      .filter(x => ids.findIndex(id => x._id.toHexString() === id))
+      .sort((a, b) => a.date.valueOf() - b.date.valueOf());
+
+    if (profiles.length !== ids.length) {
+      throw new AtNotFoundPartError("プロフィールが存在しません",
+        profiles.map(x => x._id.toString()));
+    }
+
+    return profiles.map(p => Profile.fromDB(p));
+  }
+
+  async findAll(authToken: IAuthToken): Promise<Profile[]> {
+    const profiles = this.profiles
+      .filter(x => x.user.toHexString() === authToken.user)
+      .sort((a, b) => a.date.valueOf() - b.date.valueOf());
+
+    return profiles.map(p => Profile.fromDB(p));
+  }
+
+  async insert(profile: Profile): Promise<null> {
+    this.profiles.push(profile.toDB());
+    return null;
+  }
+
+  async update(profile: Profile): Promise<null> {
+    this.profiles[this.profiles.findIndex(x => x._id.toHexString() === profile.id)] = profile.toDB();
+    return null;
+  }
+}
