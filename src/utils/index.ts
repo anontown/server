6e68f492ecc864b22ff8254a4a452bdce1107597
kgtkr <kgtkr.jp@gmail.com>
2018-03-04 18:10:08 +0900

    util

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,7 @@
 export * from "./hash";
 export * from "./mixin";
 export * from "./id-entity";
-export * from "./list";
-export * from "./option";
+import * as List from "./list";
+import * as Option from "./option";
+
+export { List, Option };
