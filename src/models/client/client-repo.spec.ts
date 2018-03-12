cdfcbe1967b649464e1d964dc25e7df709c138c6
kgtkr <kgtkr.jp@gmail.com>
2018-03-12 19:29:41 +0900

    テスト追加、修正

--- src/models/client/client-repo.spec.ts
+++ src/models/client/client-repo.spec.ts
@@ -9,16 +9,14 @@ import {
 } from "../../";
 
 function run(repoGene: () => IClientRepo, isReset: boolean) {
-  async function pre(assertions: number) {
-    expect.assertions(assertions);
+  beforeEach(async () => {
     if (isReset) {
       await dbReset();
     }
-    return repoGene();
-  }
+  });
   describe("findOne", () => {
     it("正常に探せるか", async () => {
-      const repo = await pre(1);
+      const repo = repoGene();
 
       const client = new Client(ObjectIDGenerator(),
         "name",
@@ -34,7 +32,7 @@ function run(repoGene: () => IClientRepo, isReset: boolean) {
     });
 
     it("存在しない時エラーになるか", async () => {
-      const repo = await pre(1);
+      const repo = repoGene();
 
       await repo.insert(new Client(ObjectIDGenerator(),
         "name",
@@ -47,6 +45,56 @@ function run(repoGene: () => IClientRepo, isReset: boolean) {
     });
   });
 
+  describe("findIn", () => {
+    it("正常に探せるか", async () => {
+      const repo = repoGene();
+
+      const client = new Client(ObjectIDGenerator(),
+        "name",
+        "https://hoge.com",
+        ObjectIDGenerator(),
+        new Date(0),
+        new Date(100));
+
+      const client1 = client.copy({ id: ObjectIDGenerator(), date: new Date(50) });
+      const client2 = client.copy({ id: ObjectIDGenerator(), date: new Date(80) });
+      const client3 = client.copy({ id: ObjectIDGenerator(), date: new Date(30) });
+      const client4 = client.copy({ id: ObjectIDGenerator(), date: new Date(90) });
+
+      await repo.insert(client1);
+      await repo.insert(client2);
+      await repo.insert(client3);
+      await repo.insert(client4);
+
+      expect(await repo.findIn([
+        client1.id,
+        client2.id,
+        client3.id
+      ])).toEqual([
+        client3,
+        client1,
+        client2
+      ]);
+
+      expect(await repo.findIn([])).toEqual([]);
+    });
+  });
+
+  describe("存在しない物がある時エラーになるか", async () => {
+    const repo = repoGene();
+
+    const client = new Client(ObjectIDGenerator(),
+      "name",
+      "https://hoge.com",
+      ObjectIDGenerator(),
+      new Date(0),
+      new Date(100));
+
+    await repo.insert(client);
+
+    await expect(repo.findIn([ObjectIDGenerator()])).rejects.toThrow(AtError);
+    await expect(repo.findIn([ObjectIDGenerator(), client.id])).rejects.toThrow(AtError);
+  });
 }
 
 describe("ClientRepoMock", () => {
