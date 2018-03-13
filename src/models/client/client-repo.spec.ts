9a035ff87d88c07e1a3e77bd6d7dcd9dd91cc877
kgtkr <kgtkr.jp@gmail.com>
2018-03-13 21:27:15 +0900

    テスト追加

--- src/models/client/client-repo.spec.ts
+++ src/models/client/client-repo.spec.ts
@@ -95,6 +95,60 @@ function run(repoGene: () => IClientRepo, isReset: boolean) {
       await expect(repo.findIn([ObjectIDGenerator(), client.id])).rejects.toThrow(AtError);
     });
   });
+
+  describe("findAll", () => {
+    it("正常に探せるか", async () => {
+      const repo = repoGene();
+
+      const client = new Client(ObjectIDGenerator(),
+        "name",
+        "https://hoge.com",
+        ObjectIDGenerator(),
+        new Date(0),
+        new Date(100));
+
+      const user1 = ObjectIDGenerator();
+      const user2 = ObjectIDGenerator();
+      const user3 = ObjectIDGenerator();
+
+      const client1 = client.copy({ id: ObjectIDGenerator(), user: user1, date: new Date(50) });
+      const client2 = client.copy({ id: ObjectIDGenerator(), user: user1, date: new Date(80) });
+      const client3 = client.copy({ id: ObjectIDGenerator(), user: user1, date: new Date(30) });
+      const client4 = client.copy({ id: ObjectIDGenerator(), user: user2, date: new Date(90) });
+
+      await repo.insert(client1);
+      await repo.insert(client2);
+      await repo.insert(client3);
+      await repo.insert(client4);
+
+      expect(await repo.findAll({
+        id: ObjectIDGenerator(),
+        key: "key",
+        user: user1,
+        type: "master"
+      })).toEqual([
+        client2,
+        client1,
+        client3,
+      ]);
+
+      expect(await repo.findAll({
+        id: ObjectIDGenerator(),
+        key: "key",
+        user: user2,
+        type: "master"
+      })).toEqual([
+        client4,
+      ]);
+
+      expect(await repo.findAll({
+        id: ObjectIDGenerator(),
+        key: "key",
+        user: user3,
+        type: "master"
+      })).toEqual([]);
+    });
+  });
 }
 
 describe("ClientRepoMock", () => {
