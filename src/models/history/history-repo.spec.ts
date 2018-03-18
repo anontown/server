9fc02b591d175ac6b78ab3f6b2158524d5269982
kgtkr <kgtkr.jp@gmail.com>
2018-03-18 18:58:55 +0900

    テスト追加

--- src/models/history/history-repo.spec.ts
+++ src/models/history/history-repo.spec.ts
@@ -103,6 +103,43 @@ function run(repoGene: () => IHistoryRepo, isReset: boolean) {
       await expect(repo.findIn(["history2", history.id])).rejects.toThrow(AtError);
     });
   });
+
+  describe("findAll", () => {
+    it("正常に探せるか", async () => {
+      const repo = repoGene();
+
+      const history = new History("history",
+        "topic",
+        "title",
+        Im.List(["x"]),
+        "body",
+        new Date(0),
+        "hash",
+        "user");
+
+      const history1 = history.copy({ id: "history1", topic: "topic1", date: new Date(50) });
+      const history2 = history.copy({ id: "history2", topic: "topic1", date: new Date(80) });
+      const history3 = history.copy({ id: "history3", topic: "topic1", date: new Date(30) });
+      const history4 = history.copy({ id: "history4", topic: "topic2", date: new Date(90) });
+
+      await repo.insert(history1);
+      await repo.insert(history2);
+      await repo.insert(history3);
+      await repo.insert(history4);
+
+      expect(await repo.findAll("topic1")).toEqual([
+        history2,
+        history1,
+        history3,
+      ]);
+
+      expect(await repo.findAll("topic2")).toEqual([
+        history4
+      ]);
+
+      expect(await repo.findAll("topic3")).toEqual([]);
+    });
+  });
 }
 
 describe("HistoryRepoMock", () => {
