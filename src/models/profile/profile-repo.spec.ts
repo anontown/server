36fee829d02dad1dad2ad061b462ddfc9edd8335
kgtkr <kgtkr.jp@gmail.com>
2018-03-26 14:54:02 +0900

    add test:findOne

--- src/models/profile/profile-repo.spec.ts
+++ src/models/profile/profile-repo.spec.ts
@@ -14,7 +14,38 @@ function run(repoGene: () => IProfileRepo, isReset: boolean) {
       await dbReset();
     }
   });
+  describe("findOne", () => {
+    it("正常に探せるか", async () => {
+      const repo = repoGene();
 
+      const profile = new Profile(ObjectIDGenerator(),
+        "user",
+        "name",
+        "body",
+        new Date(0),
+        new Date(10),
+        "sn");
+
+      await repo.insert(profile);
+      await repo.insert(profile.copy({ id: ObjectIDGenerator(), sn: "sn2" }));
+
+      expect(await repo.findOne(profile.id)).toEqual(profile);
+    });
+
+    it("存在しない時エラーになるか", async () => {
+      const repo = repoGene();
+
+      await repo.insert(new Profile(ObjectIDGenerator(),
+        "user",
+        "name",
+        "body",
+        new Date(0),
+        new Date(10),
+        "sn"));
+
+      await expect(repo.findOne(ObjectIDGenerator())).rejects.toThrow(AtError);
+    });
+  });
 }
 
 describe("ProfileRepoMock", () => {
