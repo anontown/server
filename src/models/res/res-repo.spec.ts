e7c080bbf95c996f8742b5ccd191fe099ebb3c88
kgtkr <kgtkr.jp@gmail.com>
2018-03-27 23:56:18 +0900

    テスト追加

--- src/models/res/res-repo.spec.ts
+++ src/models/res/res-repo.spec.ts
@@ -6,6 +6,7 @@ import {
   ResNormal,
   ResRepo,
   ResRepoMock,
+  IAuthTokenMaster,
 } from "../../";
 
 function run(repoGene: () => IResRepo, isReset: boolean) {
@@ -262,6 +263,100 @@ function run(repoGene: () => IResRepo, isReset: boolean) {
       ]);
 
       expect(await repo.findNew("topic", 0)).toEqual([]);
+    });
+  });
+
+  describe("findNotice", () => {
+    it("正常に検索出来るか", async () => {
+      const repo = repoGene();
+
+      const user1 = "user1";
+      const user2 = "user2";
+
+      const token: IAuthTokenMaster = {
+        id: "token",
+        key: "key",
+        user: user1,
+        type: "master",
+      };
+
+      const res = new ResNormal("name",
+        "body",
+        null,
+        "active",
+        null,
+        true,
+        "res",
+        "topic",
+        new Date(0),
+        user1,
+        Im.List(),
+        5,
+        "hash",
+        0,
+      );
+
+      const res1 = res.copy({ id: "res1", date: new Date(50), reply: null, user: user2 });
+      const res2 = res.copy({ id: "res2", date: new Date(80), reply: { res: "res1", user: user2 } });
+      const res3 = res.copy({ id: "res3", date: new Date(30), reply: { res: "res2", user: user1 } });
+      const res4 = res.copy({ id: "res4", date: new Date(90), reply: { res: "res2", user: user1 } });
+      const res5 = res.copy({ id: "res5", date: new Date(20), reply: { res: "res2", user: user1 } });
+      const res6 = res.copy({ id: "res6", date: new Date(10), reply: { res: "res2", user: user1 } });
+      const res7 = res.copy({ id: "res7", date: new Date(60), reply: { res: "res2", user: user1 } });
+      const res8 = res.copy({ id: "res8", date: new Date(40), reply: { res: "res2", user: user1 } });
+      const res9 = res.copy({ id: "res9", date: new Date(70), reply: { res: "res2", user: user1 } });
+
+      await repo.insert(res1);
+      await repo.insert(res2);
+      await repo.insert(res3);
+      await repo.insert(res4);
+      await repo.insert(res5);
+      await repo.insert(res6);
+      await repo.insert(res7);
+      await repo.insert(res8);
+      await repo.insert(res9);
+
+      expect(await repo.findNotice(token, "after", true, new Date(0), 100)).toEqual([
+        res4,
+        res9,
+        res7,
+        res8,
+        res3,
+        res5,
+        res6,
+      ]);
+
+      expect(await repo.findNotice(token, "after", true, new Date(70), 100)).toEqual([
+        res4,
+        res9,
+      ]);
+
+      expect(await repo.findNotice(token, "after", true, new Date(70), 1)).toEqual([
+        res9,
+      ]);
+
+      expect(await repo.findNotice(token, "after", false, new Date(70), 100)).toEqual([
+        res4,
+      ]);
+
+      expect(await repo.findNotice(token, "before", true, new Date(30), 100)).toEqual([
+        res3,
+        res5,
+        res6,
+      ]);
+
+      expect(await repo.findNotice(token, "before", true, new Date(30), 2)).toEqual([
+        res3,
+        res5,
+      ]);
+
+      expect(await repo.findNotice(token, "before", false, new Date(30), 100)).toEqual([
+        res5,
+        res6,
+      ]);
+
+      expect(await repo.findNotice(token, "after", false, new Date(90), 100)).toEqual([]);
+      expect(await repo.findNotice(token, "before", false, new Date(30), 0)).toEqual([]);
     });
   });
 }
