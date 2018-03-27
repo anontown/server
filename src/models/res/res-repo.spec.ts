cfd92a53417d3c9bc44b2174d4bfab47477ff6f3
kgtkr <kgtkr.jp@gmail.com>
2018-03-27 20:55:31 +0900

    テスト追加

--- src/models/res/res-repo.spec.ts
+++ src/models/res/res-repo.spec.ts
@@ -132,6 +132,91 @@ function run(repoGene: () => IResRepo, isReset: boolean) {
       await expect(repo.findIn(["res2", res.id])).rejects.toThrow(AtError);
     });
   });
+
+  describe("find", () => {
+    it("正常に検索出来るか", async () => {
+      const repo = repoGene();
+
+      const res = new ResNormal("name",
+        "body",
+        null,
+        "active",
+        null,
+        true,
+        "res",
+        "topic",
+        new Date(0),
+        "user",
+        Im.List(),
+        5,
+        "hash",
+        0
+      );
+
+      const res1 = res.copy({ id: "res1", date: new Date(50) });
+      const res2 = res.copy({ id: "res2", date: new Date(80), topic: "topic2" });
+      const res3 = res.copy({ id: "res3", date: new Date(30) });
+      const res4 = res.copy({ id: "res4", date: new Date(90) });
+      const res5 = res.copy({ id: "res5", date: new Date(20) });
+      const res6 = res.copy({ id: "res6", date: new Date(10) });
+      const res7 = res.copy({ id: "res7", date: new Date(60) });
+      const res8 = res.copy({ id: "res8", date: new Date(40) });
+      const res9 = res.copy({ id: "res9", date: new Date(70) });
+
+      await repo.insert(res1);
+      await repo.insert(res2);
+      await repo.insert(res3);
+      await repo.insert(res4);
+      await repo.insert(res5);
+      await repo.insert(res6);
+      await repo.insert(res7);
+      await repo.insert(res8);
+      await repo.insert(res9);
+
+      expect(await repo.find("topic", "after", true, new Date(0), 100)).toEqual([
+        res4,
+        res9,
+        res7,
+        res1,
+        res8,
+        res3,
+        res5,
+        res6,
+      ]);
+
+      expect(await repo.find("topic", "after", true, new Date(70), 100)).toEqual([
+        res4,
+        res9,
+      ]);
+
+      expect(await repo.find("topic", "after", true, new Date(70), 1)).toEqual([
+        res9,
+      ]);
+
+      expect(await repo.find("topic", "after", false, new Date(70), 100)).toEqual([
+        res4,
+      ]);
+
+      expect(await repo.find("topic", "before", true, new Date(30), 100)).toEqual([
+        res3,
+        res5,
+        res6,
+      ]);
+
+      expect(await repo.find("topic", "before", true, new Date(30), 2)).toEqual([
+        res3,
+        res5,
+      ]);
+
+      expect(await repo.find("topic", "before", false, new Date(30), 100)).toEqual([
+        res5,
+        res6,
+      ]);
+
+      expect(await repo.find("topic", "after", false, new Date(90), 100)).toEqual([]);
+      expect(await repo.find("topic", "before", false, new Date(30), 0)).toEqual([]);
+    });
+  });
 }
 
 describe("ResRepoMock", () => {
