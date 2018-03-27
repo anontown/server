4771dc54d6bdea7ad8bbbf7a040cea58b5397708
kgtkr <kgtkr.jp@gmail.com>
2018-03-27 14:16:44 +0900

    add test

--- src/models/res/res-repo.spec.ts
+++ src/models/res/res-repo.spec.ts
@@ -2,16 +2,66 @@ import {
   AtError,
   dbReset,
   IResRepo,
-  Res,
+  ResNormal,
   ResRepo,
   ResRepoMock,
 } from "../../";
+import * as Im from "immutable";
 
 function run(repoGene: () => IResRepo, isReset: boolean) {
   beforeEach(async () => {
     if (isReset) {
       await dbReset();
     }
+  });
+
+  describe("findOne", () => {
+    it("正常に探せるか", async () => {
+      const repo = repoGene();
+
+      const res = new ResNormal("name",
+        "body",
+        null,
+        "active",
+        null,
+        true,
+        "res",
+        "topic",
+        new Date(0),
+        "user",
+        Im.List(),
+        5,
+        "hash",
+        0
+      );
+
+      await repo.insert(res);
+      await repo.insert(res.copy({ id: "res2" }));
+
+      expect(await repo.findOne(res.id)).toEqual(res);
+    });
+
+    it("存在しない時エラーになるか", async () => {
+      const repo = repoGene();
+
+      await repo.insert(new ResNormal("name",
+        "body",
+        null,
+        "active",
+        null,
+        true,
+        "res",
+        "topic",
+        new Date(0),
+        "user",
+        Im.List(),
+        5,
+        "hash",
+        0
+      ));
+
+      await expect(repo.findOne("res2")).rejects.toThrow(AtError);
+    });
   });
 }
 
