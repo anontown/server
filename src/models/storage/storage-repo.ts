fbe19d6d7d5e2d3ac4edce44a3a33b2ecee02a89
kgtkr <kgtkr.jp@gmail.com>
2018-03-31 22:50:24 +0900

    StorageRepo

---
+++ src/models/storage/storage-repo.ts
@@ -0,0 +1,52 @@
+import { ObjectID } from "mongodb";
+import { IAuthToken } from "../../auth";
+import { DB } from "../../db";
+import { IStorageRepo } from "./istorage-repo";
+import { Storage, IStorageDB } from "./storage";
+import { AtNotFoundError } from "../../at-error";
+
+export class StorageRepo implements IStorageRepo {
+  async findOneKey(token: IAuthToken, key: string): Promise<Storage> {
+    const db = await DB;
+    const storage: IStorageDB | null = await db.collection("storages")
+      .findOne({
+        user: new ObjectID(token.user),
+        client: token.type === "general" ? new ObjectID(token.client) : null,
+        key
+      });
+    if (storage === null) {
+      throw new AtNotFoundError("ストレージが見つかりません");
+    }
+    return Storage.fromDB(storage);
+  }
+  async save(storage: Storage): Promise<void> {
+    const db = await DB;
+
+    await db.collection("storages")
+      .update({
+        user: new ObjectID(storage.user),
+        client: storage.client !== null ? new ObjectID(storage.client) : null,
+        key: storage.key
+      }, storage.toDB(), { upsert: true });
+  }
+  async del(storage: Storage): Promise<void> {
+    const db = await DB;
+
+    await db.collection("storages")
+      .deleteOne({
+        user: new ObjectID(storage.user),
+        client: storage.client !== null ? new ObjectID(storage.client) : null,
+        key: storage.key
+      });
+  }
+  async list(token: IAuthToken): Promise<string[]> {
+    const db = await DB;
+    const ls: IStorageDB[] = await db.collection("storages")
+      .find({
+        user: new ObjectID(token.user),
+        client: token.type === "general" ? new ObjectID(token.client) : null,
+      })
+      .toArray();
+    return ls.map(s => s.key);
+  }
+}
