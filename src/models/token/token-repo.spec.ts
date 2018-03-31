0402e8a3501abb94bfc2dee4d3ac8508a97e5a8f
kgtkr <kgtkr.jp@gmail.com>
2018-03-31 18:44:08 +0900

    テスト追加

--- src/models/token/token-repo.spec.ts
+++ src/models/token/token-repo.spec.ts
@@ -42,6 +42,58 @@ function run(repoGene: () => ITokenRepo, isReset: boolean) {
       await expect(repo.findOne(ObjectIDGenerator())).rejects.toThrow(AtError);
     });
   });
+
+  describe("findAll", () => {
+    it("正常に探せるか", async () => {
+      const repo = repoGene();
+
+      const token = new TokenMaster(ObjectIDGenerator(),
+        "key",
+        ObjectIDGenerator(),
+        new Date(0));
+
+      const user1 = ObjectIDGenerator();
+      const user2 = ObjectIDGenerator();
+      const user3 = ObjectIDGenerator();
+
+      const token1 = token.copy({ id: ObjectIDGenerator(), user: user1, date: new Date(50) });
+      const token2 = token.copy({ id: ObjectIDGenerator(), user: user1, date: new Date(80) });
+      const token3 = token.copy({ id: ObjectIDGenerator(), user: user1, date: new Date(30) });
+      const token4 = token.copy({ id: ObjectIDGenerator(), user: user2, date: new Date(90) });
+
+      await repo.insert(token1);
+      await repo.insert(token2);
+      await repo.insert(token3);
+      await repo.insert(token4);
+
+      expect(await repo.findAll({
+        id: ObjectIDGenerator(),
+        key: "key",
+        user: user1,
+        type: "master",
+      })).toEqual([
+        token2,
+        token1,
+        token3,
+      ]);
+
+      expect(await repo.findAll({
+        id: ObjectIDGenerator(),
+        key: "key",
+        user: user2,
+        type: "master",
+      })).toEqual([
+        token4,
+      ]);
+
+      expect(await repo.findAll({
+        id: ObjectIDGenerator(),
+        key: "key",
+        user: user3,
+        type: "master",
+      })).toEqual([]);
+    });
+  });
 }
 
 describe("TokenRepoMock", () => {
