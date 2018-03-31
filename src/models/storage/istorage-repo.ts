dae9304a491629f36fbb124877fa5b67dc7a5213
kgtkr <kgtkr.jp@gmail.com>
2018-03-31 22:29:53 +0900

    findOneKey

--- src/models/storage/istorage-repo.ts
+++ src/models/storage/istorage-repo.ts
@@ -2,7 +2,7 @@ import { IAuthToken } from "../../auth";
 import { Storage } from "./storage";
 
 export interface IStorageRepo {
-  findOneKey(key: string): Promise<Storage>;
+  findOneKey(token: IAuthToken, key: string): Promise<Storage>;
   save(storage: Storage): Promise<void>;
   del(storage: Storage): Promise<void>;
   list(token: IAuthToken): Promise<string[]>;
