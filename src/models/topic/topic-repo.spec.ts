8e72afd95edabb157ffc512d0bbe9656ab602331
tkr <kgtkr.jp@gmail.com>
2018-04-03 13:24:51 +0900

    テスト追加

--- src/models/topic/topic-repo.spec.ts
+++ src/models/topic/topic-repo.spec.ts
@@ -1,20 +1,39 @@
-import range from "array-range";
 import * as Im from "immutable";
 import {
-  AtError,
   dbReset,
-  IAuthTokenMaster,
   ResRepoMock,
   TopicRepoMock,
   TopicRepo,
   ITopicRepo,
+  TopicNormal
 } from "../../";
 
 function run(repoGene: () => ITopicRepo, isReset: boolean) {
+  const topicNormal = new TopicNormal("topicn",
+    "title",
+    Im.List(),
+    "body",
+    new Date(20),
+    new Date(0),
+    0,
+    new Date(10),
+    true);
+
   beforeEach(async () => {
     if (isReset) {
       await dbReset();
     }
+  });
+
+  describe("findOne", () => {
+    it("正常に取得出来るか", async () => {
+      const repo = repoGene();
+
+      await repo.insert(topicNormal.copy({ id: "topic1" }));
+      await repo.insert(topicNormal.copy({ id: "topic2" }));
+
+      expect(await repo.findOne("topic1")).toEqual(topicNormal);
+    });
   });
 }
 
