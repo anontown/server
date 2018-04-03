5b4139b1e7799992933000c2eb416438694736f4
tkr <kgtkr.jp@gmail.com>
2018-04-03 22:28:04 +0900

    テスト追加

--- src/models/topic/topic-repo.spec.ts
+++ src/models/topic/topic-repo.spec.ts
@@ -6,7 +6,7 @@ import {
   TopicRepo,
   ITopicRepo,
   TopicNormal,
-  AtError
+  AtError,
 } from "../../";
 
 function run(repoGene: () => ITopicRepo, isReset: boolean) {
@@ -100,6 +100,89 @@ function run(repoGene: () => ITopicRepo, isReset: boolean) {
         { name: "b", count: 3 },
         { name: "a", count: 2 },
         { name: "c", count: 1 }
+      ]);
+    });
+  });
+
+  describe("find", () => {
+    it("正常に検索出来るか", async () => {
+      //TODO:activeOnly
+      const repo = repoGene();
+
+      const topic1 = topicNormal.copy({
+        id: "topic1",
+        tags: Im.List(["a"]),
+        title: "x",
+        ageUpdate: new Date(10),
+      });
+
+      const topic2 = topicNormal.copy({
+        id: "topic2",
+        tags: Im.List(["a", "b"]),
+        title: "y",
+        ageUpdate: new Date(20),
+      });
+
+      const topic3 = topicNormal.copy({
+        id: "topic3",
+        tags: Im.List([]),
+        title: "x y",
+        ageUpdate: new Date(40),
+      });
+
+      const topic4 = topicNormal.copy({
+        id: "topic4",
+        tags: Im.List(["a"]),
+        title: "x",
+        ageUpdate: new Date(30),
+      });
+
+      const topic5 = topicNormal.copy({
+        id: "topic5",
+        tags: Im.List(["a"]),
+        title: "",
+        ageUpdate: new Date(50),
+      });
+
+      await repo.insert(topic1);
+      await repo.insert(topic2);
+      await repo.insert(topic3);
+      await repo.insert(topic4);
+      await repo.insert(topic5);
+
+
+      expect(await repo.find([], [], 0, 10, true)).toEqual([
+        topic5,
+        topic3,
+        topic4,
+        topic2,
+        topic1,
+      ]);
+
+      expect(await repo.find([], [], 1, 2, true)).toEqual([
+        topic3,
+        topic4,
+      ]);
+
+      expect(await repo.find([], ["a"], 0, 10, true)).toEqual([
+        topic5,
+        topic4,
+        topic2,
+        topic1,
+      ]);
+
+      expect(await repo.find([], ["a", "b"], 0, 10, true)).toEqual([
+        topic2,
+      ]);
+
+      expect(await repo.find(["x"], [], 0, 10, true)).toEqual([
+        topic3,
+        topic4,
+        topic1,
+      ]);
+
+      expect(await repo.find(["x", "y"], [], 0, 10, true)).toEqual([
+        topic3,
       ]);
     });
   });
