dadbc7beb2704bdeaff6e4bb40ff3233c0f1ce17
tkr <kgtkr.jp@gmail.com>
2018-04-03 13:32:29 +0900

    テスト追加

--- src/models/topic/topic-repo.spec.ts
+++ src/models/topic/topic-repo.spec.ts
@@ -44,6 +44,43 @@ function run(repoGene: () => ITopicRepo, isReset: boolean) {
       await expect(repo.findOne("topic2")).rejects.toThrow(AtError);
     });
   });
+
+  describe("findIn", () => {
+    it("正常に探せるか", async () => {
+      const repo = repoGene();
+
+      const topic1 = topicNormal.copy({ id: "topic1", ageUpdate: new Date(50) });
+      const topic2 = topicNormal.copy({ id: "topic2", ageUpdate: new Date(80) });
+      const topic3 = topicNormal.copy({ id: "topic3", ageUpdate: new Date(30) });
+      const topic4 = topicNormal.copy({ id: "topic4", ageUpdate: new Date(90) });
+
+      await repo.insert(topic1);
+      await repo.insert(topic2);
+      await repo.insert(topic3);
+      await repo.insert(topic4);
+
+      expect(await repo.findIn([
+        topic1.id,
+        topic2.id,
+        topic3.id,
+      ])).toEqual([
+        topic2,
+        topic1,
+        topic3,
+      ]);
+
+      expect(await repo.findIn([])).toEqual([]);
+    });
+
+    it("存在しない物がある時エラーになるか", async () => {
+      const repo = repoGene();
+
+      await repo.insert(topicNormal.copy({ id: "topic1" }));
+
+      await expect(repo.findIn(["topic2"])).rejects.toThrow(AtError);
+      await expect(repo.findIn(["topic2", "topic1"])).rejects.toThrow(AtError);
+    });
+  });
 }
 
 describe("TopicRepoMock", () => {
