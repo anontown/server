f5877d9b1de0658686cb82432e529305c2f63c1d
tkr <kgtkr.jp@gmail.com>
2018-04-05 22:23:08 +0900

    テスト追加

--- src/models/user/user-repo.spec.ts
+++ src/models/user/user-repo.spec.ts
@@ -1,14 +1,11 @@
 import {
   AtError,
-  Client,
-  ClientRepo,
-  ClientRepoMock,
   dbReset,
-  IClientRepo,
   ObjectIDGenerator,
   UserRepo,
   UserRepoMock,
   IUserRepo,
+  User,
 } from "../../";
 
 function run(repoGene: () => IUserRepo, isReset: boolean) {
@@ -16,6 +13,43 @@ function run(repoGene: () => IUserRepo, isReset: boolean) {
     if (isReset) {
       await dbReset();
     }
+  });
+
+  const user = new User(ObjectIDGenerator(),
+    "sn",
+    "pass",
+    1,
+    {
+      last: new Date(300),
+      m10: 10,
+      m30: 20,
+      h1: 30,
+      h6: 40,
+      h12: 50,
+      d1: 60,
+    },
+    new Date(100),
+    new Date(0),
+    0,
+    new Date(200));
+
+  describe("findOne", () => {
+    it("正常に取得出来るか", async () => {
+      const repo = repoGene();
+
+      await repo.insert(user);
+      await repo.insert(user.copy({ id: ObjectIDGenerator() }));
+
+      expect(await repo.findOne(user.id)).toEqual(user);
+    });
+
+    it("存在しない時エラーになるか", async () => {
+      const repo = repoGene();
+
+      await repo.insert(user);
+
+      await expect(repo.findOne(ObjectIDGenerator())).rejects.toThrow(AtError);
+    });
   });
 }
 
