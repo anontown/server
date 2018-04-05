6c424a578544d6765608899bc998eab1f0a4cb74
tkr <kgtkr.jp@gmail.com>
2018-04-06 01:19:13 +0900

    CIログ長すぎ問題修正

--- src/logger.ts
+++ src/logger.ts
@@ -27,9 +27,11 @@ log4js.configure({
             filename: "logs/app.log",
             pattern: "-yyyy-MM-dd",
         },
-        {
-            type: "console",
-        },
+        ...process.env["AT_MODE"] === "TEST"
+            ? []
+            : [{
+                type: "console",
+            }]
 
     ],
 });
