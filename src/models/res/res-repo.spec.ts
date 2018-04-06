328ceebad6179ecfb5067238d4ffdc746232f944
tkr <kgtkr.jp@gmail.com>
2018-04-06 13:29:45 +0900

    テスト修正

--- src/models/res/res-repo.spec.ts
+++ src/models/res/res-repo.spec.ts
@@ -8,6 +8,9 @@ import {
   ResNormal,
   ResRepo,
   ResRepoMock,
+  ResHistory,
+  ResTopic,
+  ResFork,
 } from "../../";
 
 function run(repoGene: () => IResRepo, isReset: boolean) {
@@ -33,14 +36,55 @@ function run(repoGene: () => IResRepo, isReset: boolean) {
     0,
   );
 
+  const resHistory = new ResHistory("history",
+    "res",
+    "topic",
+    new Date(0),
+    "user",
+    Im.List(),
+    5,
+    "hash",
+    0);
+
+  const resTopic = new ResTopic("res",
+    "topic",
+    new Date(0),
+    "user",
+    Im.List(),
+    5,
+    "hash",
+    0);
+
+  const resFork = new ResFork("topicfork",
+    "res",
+    "topic",
+    new Date(0),
+    "user",
+    Im.List(),
+    5,
+    "hash",
+    0);
+
+
+
   describe("findOne", () => {
     it("正常に探せるか", async () => {
       const repo = repoGene();
 
-      await repo.insert(resNormal);
-      await repo.insert(resNormal.copy({ id: "res2" }));
+      const res1 = resNormal.copy({ id: "res1" });
+      const res2 = resHistory.copy({ id: "res2" });
+      const res3 = resTopic.copy({ id: "res3" });
+      const res4 = resFork.copy({ id: "res4" });
 
-      expect(await repo.findOne(resNormal.id)).toEqual(resNormal);
+      await repo.insert(res1);
+      await repo.insert(res2);
+      await repo.insert(res3);
+      await repo.insert(res4);
+
+      expect(await repo.findOne(res1.id)).toEqual(res1);
+      expect(await repo.findOne(res2.id)).toEqual(res2);
+      expect(await repo.findOne(res3.id)).toEqual(res3);
+      expect(await repo.findOne(res4.id)).toEqual(res4);
     });
 
     it("存在しない時エラーになるか", async () => {
@@ -57,9 +101,9 @@ function run(repoGene: () => IResRepo, isReset: boolean) {
       const repo = repoGene();
 
       const res1 = resNormal.copy({ id: "res1", date: new Date(50) });
-      const res2 = resNormal.copy({ id: "res2", date: new Date(80) });
-      const res3 = resNormal.copy({ id: "res3", date: new Date(30) });
-      const res4 = resNormal.copy({ id: "res4", date: new Date(90) });
+      const res2 = resHistory.copy({ id: "res2", date: new Date(80) });
+      const res3 = resTopic.copy({ id: "res3", date: new Date(30) });
+      const res4 = resFork.copy({ id: "res4", date: new Date(90) });
 
       await repo.insert(res1);
       await repo.insert(res2);
@@ -94,14 +138,14 @@ function run(repoGene: () => IResRepo, isReset: boolean) {
       const repo = repoGene();
 
       const res1 = resNormal.copy({ id: "res1", date: new Date(50) });
-      const res2 = resNormal.copy({ id: "res2", date: new Date(80), topic: "topic2" });
-      const res3 = resNormal.copy({ id: "res3", date: new Date(30) });
-      const res4 = resNormal.copy({ id: "res4", date: new Date(90) });
+      const res2 = resTopic.copy({ id: "res2", date: new Date(80), topic: "topic2" });
+      const res3 = resFork.copy({ id: "res3", date: new Date(30) });
+      const res4 = resHistory.copy({ id: "res4", date: new Date(90) });
       const res5 = resNormal.copy({ id: "res5", date: new Date(20) });
-      const res6 = resNormal.copy({ id: "res6", date: new Date(10) });
+      const res6 = resTopic.copy({ id: "res6", date: new Date(10) });
       const res7 = resNormal.copy({ id: "res7", date: new Date(60) });
-      const res8 = resNormal.copy({ id: "res8", date: new Date(40) });
-      const res9 = resNormal.copy({ id: "res9", date: new Date(70) });
+      const res8 = resHistory.copy({ id: "res8", date: new Date(40) });
+      const res9 = resFork.copy({ id: "res9", date: new Date(70) });
 
       await repo.insert(res1);
       await repo.insert(res2);
@@ -165,9 +209,9 @@ function run(repoGene: () => IResRepo, isReset: boolean) {
       expect(await repo.findNew("topic", 100)).toEqual([]);
 
       const res1 = resNormal.copy({ id: "res1", date: new Date(50) });
-      const res2 = resNormal.copy({ id: "res2", date: new Date(80), topic: "topic2" });
-      const res3 = resNormal.copy({ id: "res3", date: new Date(30) });
-      const res4 = resNormal.copy({ id: "res4", date: new Date(90) });
+      const res2 = resHistory.copy({ id: "res2", date: new Date(80), topic: "topic2" });
+      const res3 = resTopic.copy({ id: "res3", date: new Date(30) });
+      const res4 = resFork.copy({ id: "res4", date: new Date(90) });
 
       await repo.insert(res1);
       await repo.insert(res2);
@@ -212,6 +256,10 @@ function run(repoGene: () => IResRepo, isReset: boolean) {
       const res7 = resNormal.copy({ id: "res7", date: new Date(60), reply: { res: "res2", user: user1 } });
       const res8 = resNormal.copy({ id: "res8", date: new Date(40), reply: { res: "res2", user: user1 } });
       const res9 = resNormal.copy({ id: "res9", date: new Date(70), reply: { res: "res2", user: user1 } });
+      const res10 = resTopic.copy({ id: "res10", date: new Date(60) });
+      const res11 = resHistory.copy({ id: "res11", date: new Date(40) });
+      const res12 = resFork.copy({ id: "res12", date: new Date(70) });
+
 
       await repo.insert(res1);
       await repo.insert(res2);
@@ -222,6 +270,9 @@ function run(repoGene: () => IResRepo, isReset: boolean) {
       await repo.insert(res7);
       await repo.insert(res8);
       await repo.insert(res9);
+      await repo.insert(res10);
+      await repo.insert(res11);
+      await repo.insert(res12);
 
       expect(await repo.findNotice(token, "after", true, new Date(0), 100)).toEqual([
         res4,
@@ -287,11 +338,17 @@ function run(repoGene: () => IResRepo, isReset: boolean) {
       const res2 = resNormal.copy({ id: "res2", date: new Date(80), reply: { res: "res1", user: user2 } });
       const res3 = resNormal.copy({ id: "res3", date: new Date(30), reply: { user: "user1", res: "res1" }, user: user2 });
       const res4 = resNormal.copy({ id: "res4", date: new Date(90), reply: { user: "user1", res: "res1" }, user: user2 });
+      const res5 = resTopic.copy({ id: "res5", date: new Date(60) });
+      const res6 = resHistory.copy({ id: "res6", date: new Date(40) });
+      const res7 = resFork.copy({ id: "res7", date: new Date(70) });
 
       await repo.insert(res1);
       await repo.insert(res2);
       await repo.insert(res3);
       await repo.insert(res4);
+      await repo.insert(res5);
+      await repo.insert(res6);
+      await repo.insert(res7);
 
       expect(await repo.findNoticeNew(token, 100)).toEqual([
         res4,
@@ -311,13 +368,13 @@ function run(repoGene: () => IResRepo, isReset: boolean) {
       const repo = repoGene();
 
       const res1 = resNormal.copy({ id: "res1", date: new Date(50), hash: "hash2" });
-      const res2 = resNormal.copy({ id: "res2", date: new Date(80), hash: "hash1" });
-      const res3 = resNormal.copy({ id: "res3", date: new Date(30) });
-      const res4 = resNormal.copy({ id: "res4", date: new Date(90) });
+      const res2 = resFork.copy({ id: "res2", date: new Date(80), hash: "hash1" });
+      const res3 = resTopic.copy({ id: "res3", date: new Date(30) });
+      const res4 = resHistory.copy({ id: "res4", date: new Date(90) });
       const res5 = resNormal.copy({ id: "res5", date: new Date(20) });
-      const res6 = resNormal.copy({ id: "res6", date: new Date(10) });
-      const res7 = resNormal.copy({ id: "res7", date: new Date(60) });
-      const res8 = resNormal.copy({ id: "res8", date: new Date(40) });
+      const res6 = resFork.copy({ id: "res6", date: new Date(10) });
+      const res7 = resTopic.copy({ id: "res7", date: new Date(60) });
+      const res8 = resHistory.copy({ id: "res8", date: new Date(40) });
       const res9 = resNormal.copy({ id: "res9", date: new Date(70) });
 
       await repo.insert(res1);
@@ -357,6 +414,9 @@ function run(repoGene: () => IResRepo, isReset: boolean) {
       const res7 = resNormal.copy({ id: "res7", date: new Date(60), reply: { res: "res2", user: "user" } });
       const res8 = resNormal.copy({ id: "res8", date: new Date(40), reply: { res: "res2", user: "user" } });
       const res9 = resNormal.copy({ id: "res9", date: new Date(70), reply: { res: "res2", user: "user" } });
+      const res10 = resTopic.copy({ id: "res10", date: new Date(60) });
+      const res11 = resHistory.copy({ id: "res11", date: new Date(40) });
+      const res12 = resFork.copy({ id: "res12", date: new Date(70) });
 
       await repo.insert(res1);
       await repo.insert(res2);
@@ -367,6 +427,9 @@ function run(repoGene: () => IResRepo, isReset: boolean) {
       await repo.insert(res7);
       await repo.insert(res8);
       await repo.insert(res9);
+      await repo.insert(res10);
+      await repo.insert(res11);
+      await repo.insert(res12);
 
       expect(await repo.findReply("res3")).toEqual([]);
       expect(await repo.findReply("res1")).toEqual([res2.copy({ replyCount: 7 })]);
@@ -399,16 +462,32 @@ function run(repoGene: () => IResRepo, isReset: boolean) {
       const repo = repoGene();
 
       const res1 = resNormal.copy({ id: "res1" });
-      const res2 = resNormal.copy({ id: "res2" });
+      const res2 = resTopic.copy({ id: "res2" });
+      const res3 = resFork.copy({ id: "res4" });
+      const res4 = resHistory.copy({ id: "res5" });
+
       const res1update = res1.copy({ body: "update" });
+      const res2update = res2.copy({ topic: "update" });
+      const res3update = res3.copy({ fork: "update" });
+      const res4update = res4.copy({ history: "update" });
 
       await repo.insert(res1);
       await repo.insert(res2);
+      await repo.insert(res3);
+      await repo.insert(res4);
 
       await repo.update(res1update);
 
-      expect(await repo.findOne(res1.id)).toEqual(res1update);
       expect(await repo.findOne(res2.id)).toEqual(res2);
+
+      await repo.update(res2update);
+      await repo.update(res3update);
+      await repo.update(res4update);
+
+      expect(await repo.findOne(res1.id)).toEqual(res1update);
+      expect(await repo.findOne(res2.id)).toEqual(res2update);
+      expect(await repo.findOne(res3.id)).toEqual(res3update);
+      expect(await repo.findOne(res4.id)).toEqual(res4update);
     });
 
     // TODO:存在しないID
@@ -418,8 +497,20 @@ function run(repoGene: () => IResRepo, isReset: boolean) {
     it("正常に取得出来るか", async () => {
       const repo = repoGene();
 
-      for (let i = 0; i < 100; i++) {
+      for (let i = 0; i < 25; i++) {
         await repo.insert(resNormal.copy({ id: "res" + i, topic: "topic1" }));
+      }
+
+      for (let i = 25; i < 50; i++) {
+        await repo.insert(resHistory.copy({ id: "res" + i, topic: "topic1" }));
+      }
+
+      for (let i = 50; i < 75; i++) {
+        await repo.insert(resTopic.copy({ id: "res" + i, topic: "topic1" }));
+      }
+
+      for (let i = 75; i < 100; i++) {
+        await repo.insert(resFork.copy({ id: "res" + i, topic: "topic1" }));
       }
 
       await repo.insert(resNormal.copy({ id: "resres", topic: "topic2" }));
@@ -440,6 +531,18 @@ function run(repoGene: () => IResRepo, isReset: boolean) {
 
       for (const i of range(1, 100)) {
         await repo.insert(resNormal.copy({ id: "res" + i, reply: { user: "user", res: "res" + (i - 1) } }));
+      }
+
+      for (let i = 100; i < 125; i++) {
+        await repo.insert(resHistory.copy({ id: "res" + i, topic: "topic1" }));
+      }
+
+      for (let i = 125; i < 150; i++) {
+        await repo.insert(resTopic.copy({ id: "res" + i, topic: "topic1" }));
+      }
+
+      for (let i = 150; i < 175; i++) {
+        await repo.insert(resFork.copy({ id: "res" + i, topic: "topic1" }));
       }
 
       expect(await repo.replyCount([])).toEqual(new Map());
