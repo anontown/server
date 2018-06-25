3b787580d6e78f60d9f3f965d5c830ef9563de18
tkr <kgtkr.jp@gmail.com>
2018-06-25 19:03:39 +0900

    createServer

--- src/server/server.ts
+++ src/server/server.ts
@@ -4,14 +4,18 @@ import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
 import { buildSchema } from "graphql";
 import * as fs from "fs";
 import { Config } from "../config";
+import * as http from "http";
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+const server = http.createServer(app as any);
+
 app.use("/graphql", graphqlExpress({
   schema: buildSchema(fs.readFileSync("app.graphql", "utf8"))
 }));
 app.get("/graphiql", graphiqlExpress({ endpointURL: "/graphql" }));
 
-app.listen(Config.server.port);
+server.listen(Config.server.port);
