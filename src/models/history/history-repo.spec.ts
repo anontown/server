b0a116398742cca21ce75f1e6a6a3ae4eadf6f8c
tkr <kgtkr.jp@gmail.com>
2018-07-04 16:38:43 +0900

    test history find

--- src/models/history/history-repo.spec.ts
+++ src/models/history/history-repo.spec.ts
@@ -141,6 +141,70 @@ function run(repoGene: () => IHistoryRepo, isReset: boolean) {
     });
   });
 
+  describe("find", () => {
+    it("正常に検索出来るか", async () => {
+      const repo = repoGene();
+
+      const history = new History("history",
+        "topic",
+        "title",
+        Im.List(["x"]),
+        "text",
+        new Date(0),
+        "hash",
+        "user");
+
+      const history1 = history.copy({ id: "history1", topic: "topic1", date: new Date(50) });
+      const history2 = history.copy({ id: "history2", topic: "topic1", date: new Date(80) });
+      const history3 = history.copy({ id: "history3", topic: "topic1", date: new Date(30) });
+      const history4 = history.copy({ id: "history4", topic: "topic2", date: new Date(90) });
+
+      await repo.insert(history1);
+      await repo.insert(history2);
+      await repo.insert(history3);
+      await repo.insert(history4);
+
+      expect(await repo.find({ topic: null, id: null })).toEqual([
+        history4,
+        history2,
+        history1,
+        history3,
+      ]);
+
+      expect(await repo.find({ topic: ["topic1"], id: null })).toEqual([
+        history2,
+        history1,
+        history3,
+      ]);
+
+      expect(await repo.find({ topic: ["topic2"], id: null })).toEqual([
+        history4,
+      ]);
+
+      expect(await repo.find({ topic: ["topic3"], id: null })).toEqual([]);
+
+      expect(await repo.find({ topic: [], id: null })).toEqual([]);
+
+      expect(await repo.find({ topic: ["topic1", "topic2"], id: null })).toEqual([
+        history4,
+        history2,
+        history1,
+        history3,
+      ]);
+
+      expect(await repo.find({ topic: null, id: [] })).toEqual([]);
+      expect(await repo.find({ topic: null, id: ["history2", "a"] })).toEqual([history2]);
+      expect(await repo.find({ topic: null, id: ["history2", "history4"] })).toEqual([
+        history4,
+        history2
+      ]);
+
+      expect(await repo.find({ topic: ["topic1"], id: ["history2", "history4"] })).toEqual([
+        history2
+      ]);
+    });
+  });
+
   describe("insert", () => {
     it("正常に保存出来るか", async () => {
       const repo = repoGene();
