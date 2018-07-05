70574b1a1609e030923e3fb7ea5ef3e75efc584d
tkr <kgtkr.jp@gmail.com>
2018-07-05 19:46:01 +0900

    gql profile

--- src/controllers/profile.ts
+++ src/controllers/profile.ts
@@ -7,7 +7,50 @@ import {
   controller,
   http,
   IHttpAPICallParams,
+  Context,
 } from "../server";
+
+export const profileResolver = {
+  Query: {
+    profiles: async (_obj: any,
+      args: {
+        id: string[] | null,
+        self: boolean | null,
+      }, context: Context,
+      _info: any) => {
+      const profiles = await context.repo.profile.find(context.auth, { id: args.id, self: args.self });
+      return profiles.map(p => p.toAPI(context.auth.tokenOrNull));
+    },
+  },
+  Mutation: {
+    createProfile: async (_obj: any,
+      args: {
+        name: string,
+        text: string,
+        sn: string
+      }, context: Context,
+      _info: any) => {
+      const profile = Profile.create(ObjectIDGenerator, context.auth.token, args.name, args.text, args.sn, context.now);
+      await context.repo.profile.insert(profile);
+      context.log("profiles", profile.id);
+      return profile.toAPI(context.auth.token);
+    },
+    updateProfile: async (_obj: any,
+      args: {
+        id: string,
+        name: string,
+        text: string,
+        sn: string
+      }, context: Context,
+      _info: any) => {
+      const profile = await context.repo.profile.findOne(args.id);
+      const newProfile = profile.changeData(context.auth.token, args.name, args.text, args.sn, context.now);
+      await context.repo.profile.update(newProfile);
+      context.log("profiles", newProfile.id);
+      return newProfile.toAPI(context.auth.token);
+    },
+  },
+};
 
 @controller
 export class ProfileController {
