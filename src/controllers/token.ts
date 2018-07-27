ab5468fcd42096a4807bcbd84c2a1a9f47be338e
kgtkr <kgtkr.jp@gmail.com>
2018-07-27 20:53:22 +0900

    tokenResolver

--- src/controllers/token.ts
+++ src/controllers/token.ts
@@ -12,7 +12,81 @@ import {
   controller,
   http,
   IHttpAPICallParams,
+  Context,
 } from "../server";
+
+export const tokenResolver = {
+  Query: {
+    token: async (_obj: any,
+      _args: {}, context: Context,
+      _info: any) => {
+      const token = await context.repo.token.findOne(context.auth.token.id);
+      return token.toAPI();
+    },
+    tokens: async (_obj: any,
+      _args: {}, context: Context,
+      _info: any) => {
+      const tokens = await context.repo.token.findAll(context.auth.tokenMaster);
+      return tokens.map(t => t.toAPI());
+    },
+  },
+  Mutation: {
+    delTokenClient: async (_obj: any,
+      args: {
+        client: string
+      }, context: Context,
+      _info: any) => {
+      const client = await context.repo.client.findOne(args.client);
+      await context.repo.token.delClientToken(context.auth.tokenMaster, client.id);
+      return null;
+    },
+    createTokenGeneral: async (_obj: any,
+      args: {
+        client: string
+      }, context: Context,
+      _info: any) => {
+      const client = await context.repo.client.findOne(args.client);
+      const token = TokenGeneral.create(ObjectIDGenerator, context.auth.tokenMaster, client, context.now, RandomGenerator);
+      await context.repo.token.insert(token);
+
+      return token.toAPI();
+    },
+    createTokenMaster: async (_obj: any,
+      _args: {}, context: Context,
+      _info: any) => {
+      const token = TokenMaster.create(ObjectIDGenerator, context.auth.user, context.now, RandomGenerator);
+      await context.repo.token.insert(token);
+
+      return token.toAPI();
+    },
+    createTokenReq: async (_obj: any,
+      _args: {}, context: Context,
+      _info: any) => {
+      const token = await context.repo.token.findOne(context.auth.token.id);
+      if (token.type !== "general") {
+        throw new AtPrerequisiteError("通常トークン以外では出来ません");
+      }
+      const { req, token: newToken } = token.createReq(context.now, RandomGenerator);
+
+      await context.repo.token.update(newToken);
+
+      return req;
+    },
+    authTokenReq: async (_obj: any,
+      args: {
+        id: string,
+        key: string
+      }, context: Context,
+      _info: any) => {
+      const token = await context.repo.token.findOne(args.id);
+      if (token.type !== "general") {
+        throw new AtPrerequisiteError("通常トークン以外では出来ません");
+      }
+      token.authReq(args.key, context.now);
+      return token.toAPI();
+    },
+  },
+};
 
 @controller
 export class TokenController {
