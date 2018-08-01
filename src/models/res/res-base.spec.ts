30d18efe5dddb952a8242e14ed0be238ce2c9ac6
kgtkr <kgtkr.jp@gmail.com>
2018-08-01 20:09:22 +0900

    fix test

--- src/models/res/res-base.spec.ts
+++ src/models/res/res-base.spec.ts
@@ -214,7 +214,7 @@ describe("ResBase", () => {
         id: res.id,
         topic: res.topic,
         date: res.date,
-        user: false,
+        self: false,
         uv: 1,
         dv: 1,
         hash: res.hash,
