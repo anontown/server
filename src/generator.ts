5c1ac60c58f946445262fe9a6f1880eb50b153fe
kgtkr <kgtkr.jp@gmail.com>
2018-10-06 16:14:09 +0900

    nanoid

--- src/generator.ts
+++ src/generator.ts
@@ -1,7 +1,8 @@
 import { ObjectID } from "mongodb";
+import nanoid from "nanoid";
 
 export type IGenerator<T> = () => T;
 
 export const ObjectIDGenerator: IGenerator<string> = () => new ObjectID().toString();
 
-export const RandomGenerator: IGenerator<string> = () => String(Math.random());
+export const RandomGenerator: IGenerator<string> = () => nanoid();
