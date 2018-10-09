c53b730fbd42ad63ae03e07e01e3e7348c39249a
kgtkr <kgtkr.jp@gmail.com>
2018-10-09 20:11:55 +0900

    del-undef

---
+++ src/utils/del-undef.ts
@@ -0,0 +1,10 @@
+export function delUndef<T>(obj: T): T {
+  const res: any = {};
+  for (const key of Object.keys(obj)) {
+    const val = (obj as any)[key];
+    if (val !== undefined) {
+      res[key] = val;
+    }
+  }
+  return res;
+}
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,7 @@
 export * from "./hash";
 export * from "./mixin";
 export * from "./id-entity";
+export * from "./del-undef";
 import * as List from "./list";
 import * as Option from "./option";
 export { List, Option };
