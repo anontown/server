b627057b3c701695bbd79ca3263cd99c899f12a6
kgtkr <kgtkr.jp@gmail.com>
2018-10-10 20:42:21 +0900

    テスト設定

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,15 @@
+const ioMode = process.env["TEST_MODE"] === "IO";
+
 module.exports = {
   transform: {
     "^.+\\.tsx?$": "ts-jest"
   },
   testMatch: [
     "**/src/**/*.spec.ts"
+  ],
+  testPathIgnorePatterns: [
+    "/node_modules/",
+    ...(!ioMode ? ["**/src/**/*.spec.io.ts"] : [])
   ],
   moduleFileExtensions: [
     "ts",
