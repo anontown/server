5c066ce65b237b5625a7eccdcb425fc1e32371cf
kgtkr <kgtkr.jp@gmail.com>
2018-10-13 14:22:56 +0900

    validate

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,5 @@
+import { validateData } from "./utils/index";
+
 export const Config = {
     timezone: "Asia/Tokyo",
     server: {
@@ -22,30 +24,36 @@ export const Config = {
     user: {
         sn: {
             regex: /^[a-zA-Z0-9_]{3,20}$/,
+            validate: validateData(["lc", "uc", "d", "ub"], 3, 20),
             msg: "スクリーンネームは半角英数字、アンダーバー3～20文字にして下さい",
         },
         pass: {
             regex: /^[a-zA-Z0-9_]{3,50}$/,
+            validate: validateData(["lc", "uc", "d", "ub"], 3, 50),
             msg: "パスワードは半角英数字、アンダーバー3～50文字にして下さい",
         },
         lvMax: 1000,
         profile: {
             name: {
                 regex: /^.{1,50}$/,
+                validate: validateData(null, 1, 50),
                 msg: "名前は1～50文字にして下さい",
             },
             sn: {
                 regex: /^[a-zA-Z0-9_]{3,20}$/,
+                validate: validateData(["lc", "uc", "d", "ub"], 3, 20),
                 msg: "スクリーンネームは半角英数字、アンダーバー3～20文字にして下さい",
             },
             text: {
                 regex: /^[\s\S]{1,3000}$/,
+                validate: validateData(null, 1, 3000),
                 msg: "自己紹介文は1～3000文字にして下さい",
             },
         },
         token: {
             name: {
                 regex: /^.{1,50}$/,
+                validate: validateData(null, 1, 50),
                 msg: "名前は1～50文字にして下さい",
             },
             req: {
@@ -55,16 +63,19 @@ export const Config = {
         storage: {
             key: {
                 regex: /^.{1,100}$/,
+                validate: validateData(null, 1, 100),
                 msg: "ストレージキーは1～100文字にして下さい",
             },
             value: {
                 regex: /^.{0,100000}$/,
+                validate: validateData(null, 0, 100000),
                 msg: "ストレージの値は0～100000文字にして下さい",
             },
         },
         client: {
             name: {
                 regex: /^.{1,30}$/,
+                validate: validateData(null, 1, 30),
                 msg: "名前は1～30文字にして下さい",
             },
             url: {
@@ -76,15 +87,18 @@ export const Config = {
     topic: {
         title: {
             regex: /^.{1,100}$/,
+            validate: validateData(null, 1, 100),
             msg: "タイトルは1～100文字にして下さい",
         },
         tags: {
             regex: /^[a-z0-9ぁ-んァ-ヶー一-龠々_]{1,20}$/,
+            validate: validateData(["lc", "d", "hira", "kana", "han", "ub"], 1, 20),
             max: 15,
             msg: "タグは半角小文字英数字ひらがな漢字、アンダーバー1～20文字15個以内にして下さい",
         },
         text: {
             regex: /^[\s\S]{1,10000}$/,
+            validate: validateData(null, 1, 10000),
             msg: "本文は1～1万文字以内にして下さい",
         },
         hashLen: 6,
@@ -92,10 +106,12 @@ export const Config = {
     res: {
         name: {
             regex: /^.{1,50}$/,
+            validate: validateData(null, 1, 50),
             msg: "名前は50文字以内にして下さい",
         },
         text: {
             regex: /^[\s\S]{1,5000}$/,
+            validate: validateData(null, 1, 5000),
             msg: "本文は1～5000文字にして下さい",
         },
         wait: {
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,6 +3,7 @@ export * from "./mixin";
 export * from "./id-entity";
 export * from "./del-undef";
 export * from "./copyable";
+export * from "./string-validate";
 import * as List from "./list";
 import * as Option from "./option";
 export { List, Option };
