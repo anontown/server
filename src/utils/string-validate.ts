1e4e1626014c1d176e0c39905267d9e5c3d7efa1
kgtkr <kgtkr.jp@gmail.com>
2018-10-13 09:33:02 +0900

    charTypeToReg

--- src/utils/string-validate.ts
+++ src/utils/string-validate.ts
@@ -7,12 +7,37 @@ ub:アンダーバー
 hy:ハイフン
 hira:ひらがな
 kana:カタカナ
-hn:漢字
+han:漢字
 */
-export type CharType = "lc" | "uc" | "d" | "ub" | "hy" | "hira" | "kana" | "hn";
+export type CharType = "lc" | "uc" | "d" | "ub" | "hy" | "hira" | "kana" | "han";
 
 export interface ValidateData {
   char: CharType,
   min: number | null,
   max: number | null,
+}
+
+function charTypeToReg(type: CharType): string {
+  switch (type) {
+    case "lc":
+      return "a-z";
+    case "uc":
+      return "A-Z";
+    case "d":
+      return "0-9";
+    case "ub":
+      return "_";
+    case "hy":
+      return "-";
+    case "hira":
+      return "\\p{Script_Extensions=Hiragana}";
+    case "kana":
+      return "\\p{Script_Extensions=Katakana}";
+    case "han":
+      return "\\p{Script_Extensions=Han}";
+  };
+}
+
+function validateToReg(data: ValidateData): RegExp {
+
 }
