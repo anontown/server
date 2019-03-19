30be87c7b483d6276c84645f8af0adb3ccac5487
kgtkr <kgtkr.jp@gmail.com>
2019-03-19 12:21:10 +0900

    バグ修正

--- start-dev.sh
+++ start-dev.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 export $(cat .env | xargs)
 export SERVER_PORT=8080
-export DB_URL=mongodb://mongo:27017/anontown
-export ES_HOST=es:9200
+export DB_URL=mongodb://localhost:27017/anontown
+export ES_HOST=localhost:9200
 
 cd ./data/app/
 
