87ec0b6d7e9919771ea6a8012f291cfc752a6cdf
kgtkr <kgtkr.jp@gmail.com>
2019-03-21 02:32:34 +0900

    add nullToUndefined

--- src/utils/is-null-or-undefined.ts
+++ src/utils/is-null-or-undefined.ts
@@ -1,3 +1,7 @@
 export function isNullOrUndefined<T>(x: T | null | undefined): x is null | undefined {
   return x === null || x === undefined;
 }
+
+export function nullToUndefined<T>(x: T | null | undefined): T | undefined {
+  return x === null ? undefined : x;
+}
