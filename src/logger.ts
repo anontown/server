c7588b49f7759629349c83f6714b84776f410d4e
kgtkr <kgtkr.jp@gmail.com>
2019-04-21 21:39:11 +0900

    ログ周りのバグ修正

--- src/logger.ts
+++ src/logger.ts
@@ -5,6 +5,11 @@ import { Config } from "./config";
 // log
 log4js.configure({
     appenders: {
+        default: {
+            type: "dateFile",
+            filename: path.join(Config.saveDir, "logs/default.log"),
+            pattern: "-yyyy-MM-dd",
+        },
         system: {
             type: "dateFile",
             filename: path.join(Config.saveDir, "logs/system.log"),
@@ -26,7 +31,13 @@ log4js.configure({
             pattern: "-yyyy-MM-dd",
         },
     },
-    categories: { default: { appenders: ["system", "access", "error", "app"], level: "info" } },
+    categories: {
+        default: { appenders: ["default"], level: "info" },
+        system: { appenders: ["system"], level: "info" },
+        access: { appenders: ["access"], level: "info" },
+        error: { appenders: ["error"], level: "info" },
+        app: { appenders: ["app"], level: "info" },
+    },
 });
 
 export class Logger {
