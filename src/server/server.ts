64994ad735513c92f7c4cb9628a97c3a0554bb8f
kgtkr <kgtkr.jp@gmail.com>
2019-04-23 12:36:00 +0900

    expressを使うように

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,5 +1,5 @@
 import { combineResolvers } from "apollo-resolvers";
-import { ApolloServer, gql, IResolvers } from "apollo-server";
+import { ApolloServer, gql, IResolvers } from "apollo-server-express";
 import * as fs from "fs";
 import {
   GraphQLDateTime,
@@ -9,6 +9,7 @@ import { Config } from "../config";
 import { IRepo } from "../models";
 import { resolvers as appResolvers } from "../resolvers";
 import { AppContext, createContext } from "./context";
+import * as express from "express";
 
 export async function serverRun(repo: IRepo) {
   const typeDefs = gql(fs.readFileSync("node_modules/@anontown/schema/app.gql", "utf8"));
@@ -54,7 +55,10 @@ export async function serverRun(repo: IRepo) {
 
   repo.cron();
 
-  await server.listen(Config.server.port).then(({ url, subscriptionsUrl }) => {
-    console.log(`Server ready at ${url} ${subscriptionsUrl}`);
+  const app = express();
+  server.applyMiddleware({ app });
+
+  app.listen({ port: Config.server.port }, () => {
+    console.log(`Server ready at ${server.graphqlPath}, ${server.subscriptionsPath}`);
   });
 }
