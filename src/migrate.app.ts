6f1a8f14d93638a173591425549aab8a1e02fbb8
kgtkr <kgtkr.jp@gmail.com>
2019-04-24 21:13:37 +0900

    exit

--- src/migrate.app.ts
+++ src/migrate.app.ts
@@ -1,5 +1,11 @@
 import { migrateUp } from "./utils";
 
 migrateUp()
-  .then(() => console.log("done"))
-  .catch(e => console.error("error", e));
+  .then(() => {
+    console.log("done");
+    process.exit(0);
+  })
+  .catch(e => {
+    console.error("error", e);
+    process.exit(-1);
+  });
