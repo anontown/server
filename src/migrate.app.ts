feccbf14af07fd1897808161ea3691e2b631b75d
kgtkr <kgtkr.jp@gmail.com>
2019-04-24 17:17:59 +0900

    バグ修正

--- src/migrate.app.ts
+++ src/migrate.app.ts
@@ -1,6 +1,7 @@
 import { migrateSet } from "./utils";
+import { promisify } from "util";
 
 migrateSet
-  .then(x => x.up())
+  .then(x => promisify(x.up).bind(x)())
   .then(() => console.log("done"))
   .catch(e => console.error("error", e));
