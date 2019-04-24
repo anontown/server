7d42188dabf14ad9a46b0263183998ea3ab090ca
kgtkr <kgtkr.jp@gmail.com>
2019-04-24 12:27:33 +0900

    dev-env

--- start-dev.sh
+++ start-dev.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 export $(cat .env | xargs)
-export SERVER_PORT=8080
-export DB_URL=mongodb://localhost:27017/anontown
-export ES_HOST=localhost:9200
-export SAVE_DIR=./data/app/
-export REDIS_URL=redis://localhost:6379/0
+export $(cat dev-env | xargs)
 
 node ./dist/app.js
